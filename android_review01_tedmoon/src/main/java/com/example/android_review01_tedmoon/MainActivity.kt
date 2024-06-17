4efba4cfe62794a08c2e2f17a1bd09328e27d7f9
TedMoon99 <mtj1015@naver.com>
2024-06-17 17:40:10 +0900

    [Feat] MainActivity의 FragmentContainer에  MainFragment로 화면전환 기능 적용

--- android_review01_tedmoon/src/main/java/com/example/android_review01_tedmoon/MainActivity.kt
+++ android_review01_tedmoon/src/main/java/com/example/android_review01_tedmoon/MainActivity.kt
@@ -20,6 +20,12 @@ class MainActivity : AppCompatActivity() {
 
     // 화면 이동
     fun startDevice(){
+        supportFragmentManager
+            .beginTransaction() // 트랜잭션 생성
+            .replace(R.id.containerMain, MainFragment()) // FragmentContainerView -> MainFragment 화면 출력
+//            .addToBackStack(false) 화면전환 시에는 BackStack에 추가해야 되는 것이 맞지만 가장 기본이 되는 Fragment는 추가하지 않는다
+            .commit() // 실행
+
 
 
     }
