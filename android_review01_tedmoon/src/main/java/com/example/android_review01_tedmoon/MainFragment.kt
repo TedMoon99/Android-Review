8ca455f2eab077820838e29c902e2107e02cb843
TedMoon99 <mtj1015@naver.com>
2024-06-17 17:53:38 +0900

    [Feat] MainFragment - onViewCreated 생성

--- android_review01_tedmoon/src/main/java/com/example/android_review01_tedmoon/MainFragment.kt
+++ android_review01_tedmoon/src/main/java/com/example/android_review01_tedmoon/MainFragment.kt
@@ -10,6 +10,8 @@ import com.example.android_review01_tedmoon.databinding.FragmentMainBinding
 class MainFragment : Fragment() {
 
     private lateinit var binding: FragmentMainBinding
+
+    // 바인딩 괸련된 코드는 onCreateView에 작성해주는 것이 좋고,
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -17,5 +19,11 @@ class MainFragment : Fragment() {
 
         binding = FragmentMainBinding.inflate(inflater)
         return binding.root
+    }
+
+    // View와 관련된 코드는 onViewCreated에 작성해주는 것이 좋다. - 생명주기(lifecycler)과 관련이 있다
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 }
