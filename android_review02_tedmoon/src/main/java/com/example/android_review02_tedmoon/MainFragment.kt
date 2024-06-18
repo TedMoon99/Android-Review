0cc1f4fa7e0d627a7bfd060899662f9ae5615380
TedMoon99 <mtj1015@naver.com>
2024-06-18 17:22:13 +0900

    [Feat] MainFragment 1. binding 추가 2. onViewCreated 추가

--- android_review02_tedmoon/src/main/java/com/example/android_review02_tedmoon/MainFragment.kt
+++ android_review02_tedmoon/src/main/java/com/example/android_review02_tedmoon/MainFragment.kt
@@ -5,13 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.android_review02_tedmoon.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+
+        binding = FragmentMainBinding.inflate(inflater)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
