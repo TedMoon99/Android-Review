0c4b222300f3ea7e9edd652b5ac8e05c3f3aadde
baek08102 <baek2654@gmail.com>
2024-06-19 00:32:07 +0900

    fragment_main.xml 완료

---
+++ android_review02_baek08102/src/main/java/com/example/android_review02_baek08102/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.android_review02_baek08102
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+    }
+
+    //
+}
---
+++ android_review02_baek08102/src/main/java/com/example/android_review02_baek08102/MyAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.android_review02_baek08102
+
+class MyAdapter {
+}
