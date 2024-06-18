03c1b26ade8e005213fea3061c1258a49ac5ab5c
TedMoon99 <mtj1015@naver.com>
2024-06-18 19:52:42 +0900

    [Feat]
    MainFragment
    1. Button 클릭시 RecyclerView에 항목 들어가는 것을 확인
    2. 유효성 검사 추가 확인
    
    fragment_main.xml
    1. 에러 설정 확인
    
    row_main.xml
    1. height 크기 조정 match_parent -> wrap_content

--- android_review02_tedmoon/src/main/java/com/example/android_review02_tedmoon/MainFragment.kt
+++ android_review02_tedmoon/src/main/java/com/example/android_review02_tedmoon/MainFragment.kt
@@ -57,7 +57,44 @@ class MainFragment : Fragment() {
     // Event 설정
     fun settingEvent(){
         binding.apply {
+            // 버튼 클릭시 이름 항목 RecyclerView에 추가
+            buttonMainAdd.setOnClickListener {
+                val resultCheck = validateInput()
 
+                if (resultCheck){ // 유효성 검사 통과시
+                    // 이름 값을 받아온다
+                    val name = textInputEditTextMainName.text.toString()
+                    // dataList에 값을 넣어준다
+                    dataList.add(name)
+                    // Adapter에게 값이 변경되었음을 알려준다
+                    recyclerViewMain.adapter?.notifyItemInserted(dataList.size - 1)
+                }
+            }
+
+            // 텍스트 체인지 리스너 사용
         }
+    }
+
+    // 유효성 검사
+    fun validateInput(): Boolean{
+        var resultName = false
+        binding.apply {
+            // 입력된 이름을 받아온다
+            val name = textInputEditTextMainName.text.toString()
+
+            if (name.isNotEmpty()){ // 이름이 비어있지 않다면
+                if (name.length in 2 until 6){ // 정상적인 입력
+                    textInputLayoutMainName.error = null
+                    resultName = true
+                } else{ // 비정상적인 입력
+                    textInputLayoutMainName.error = "잘못된 입력입니다. 이름은 2자 ~ 5자로 입력해주세요"
+                    resultName = false
+                }
+            } else { // 이름이 비어있는 경우
+                textInputLayoutMainName.error = "이름을 입력해주세요"
+                resultName = false
+            }
+        }
+        return resultName
     }
 }
