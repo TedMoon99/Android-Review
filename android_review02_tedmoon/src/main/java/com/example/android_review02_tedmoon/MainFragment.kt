802c3c384573a73e700b44f38f1d5ac0dcef393b
TedMoon99 <mtj1015@naver.com>
2024-06-18 21:04:07 +0900

    [Feat] 필터링 기능 추가

--- android_review02_tedmoon/src/main/java/com/example/android_review02_tedmoon/MainFragment.kt
+++ android_review02_tedmoon/src/main/java/com/example/android_review02_tedmoon/MainFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.android_review02_tedmoon.databinding.FragmentMainBinding
 import com.google.android.material.divider.MaterialDividerItemDecoration
 
@@ -14,6 +16,8 @@ class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
     // RecyclerView에 전달할 데이터리스트
     var dataList: MutableList<String> = mutableListOf()
+    // 검색 결과를 나타낼 새로운 목록
+    private val filteredList: MutableList<String> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -67,11 +71,47 @@ class MainFragment : Fragment() {
                     // dataList에 값을 넣어준다
                     dataList.add(name)
                     // Adapter에게 값이 변경되었음을 알려준다
-                    recyclerViewMain.adapter?.notifyItemInserted(dataList.size - 1)
+                    recyclerViewMain.adapter?.notifyDataSetChanged()
+
+                    // textField 이름을 초기화 해준다
+                    textInputEditTextMainName.text = null
                 }
             }
 
-            // 텍스트 체인지 리스너 사용
+            // 텍스트 체인지 리스너
+            // 값이 변화할 때마다 작동하는 리스너
+            textInputEditTextMainSearch.addTextChangedListener { search ->
+                // 비어있는 경우 모든 항목이 나오게 한다
+                if (search == null){
+                    recyclerViewMain.apply {
+
+                        // adapter dataList 전송
+                        adapter = CustomAdapter(dataList)
+
+                        // RecyclerView의 어댑터에게 변경을 알림
+                        adapter?.notifyDataSetChanged()
+                    }
+                }
+                // 비어있지 않은 경우
+                else {
+                    // adapter filteredList 전송
+                    recyclerViewMain.adapter = CustomAdapter(filteredList)
+
+                    // 검색어에 따라 필터링하여 새로운 목록 생성
+                    val newFilteredList = dataList.filter {
+                        it.contains(search.toString())
+                    }
+
+                    // 이전 검색 결과 목록을 제거
+                    filteredList.clear()
+
+                    // 새롭게 필터링된 목록을 리스트에 전송
+                    filteredList.addAll(newFilteredList)
+
+                    // RecyclerView에 새로운 필터링 목록을 전송하고 어댑터에게 변경 알림
+                    recyclerViewMain.adapter?.notifyDataSetChanged()
+                }
+            }
         }
     }
 
