9653de0c0ef74d531dd174d8da0abcdb363e65f2
baek08102 <baek2654@gmail.com>
2024-06-21 01:23:59 +0900

    [Feat] MainActivity - 화면 생성 함수 세팅

--- android_review02_baek08102/src/main/java/com/example/android_review02_baek08102/MainActivity.kt
+++ android_review02_baek08102/src/main/java/com/example/android_review02_baek08102/MainActivity.kt
@@ -5,16 +5,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.android_review02_baek08102.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        startDevice()
+    }
+
+    // 화면 시작 위한 함수 세팅
+    fun startDevice() {
+        supportFragmentManager // 프래그먼트 추가, 교체, 제거하는 작업 수행
+            .beginTransaction() // 새로운 트랜잭션 생성
+            .replace(R.id.main_container, MainFragment()) // FragmentContainerView를 MainFragment로 교체
+            .commit() // 트랜잭션 실행
     }
 }
