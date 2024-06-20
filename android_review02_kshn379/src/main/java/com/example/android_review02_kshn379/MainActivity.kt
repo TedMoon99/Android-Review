dbbbe3253fd0d41c1b7d6a091892cc1d1cfbde29
KSHN <koorain@naver.com>
2024-06-20 23:12:47 +0900

    KSH_Review02

--- android_review02_kshn379/src/main/java/com/example/android_review02_kshn379/MainActivity.kt
+++ android_review02_kshn379/src/main/java/com/example/android_review02_kshn379/MainActivity.kt
@@ -1,32 +1,19 @@
 package com.example.android_review02_kshn379
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android_review02_kshn379.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
-    val dataList: MutableList<Research> = mutableListOf()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // 바인딩 초기화
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        // 뷰 설정
-        settingView()
-        // 이벤트 설정
-        settingEvent()
-
-    }
-
 
     // 이벤트 설정
     fun settingEvent() {
@@ -47,8 +34,41 @@ class MainActivity : AppCompatActivity() {
                     recyclerViewMain.adapter?.notifyDataSetChanged()
                 }
             }
+
+            textInputEditTextMainSearch.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    Log.d("test1234","s : ${s}")
+                    Log.d("test1234","start : ${start}")
+                    Log.d("test1234","before : ${before}")
+                    Log.d("test1234","count : ${count}")
+                    val searchText = s.toString()
+
+                    val list = dataList.filter {
+                        it.name.contains(searchText)
+                    }
+
+                    filteredList = list
+
+                    filterData(searchText)
+                }
+
+                private fun filterData(searchText: String) {
+                    searchText
+
+
+                }
+                override fun afterTextChanged(s: Editable?) {
+
+                }
+            })
         }
     }
+
+
 
     // 뷰 설정
     fun settingView() {
@@ -58,6 +78,23 @@ class MainActivity : AppCompatActivity() {
                 layoutManager = LinearLayoutManager(context)
             }
         }
+
+    }
+
+    var filteredList : MutableList<Research> = mutableListOf()
+    val dataList: MutableList<Research> = mutableListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 바인딩 초기화
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // 뷰 설정
+        settingView()
+        // 이벤트 설정
+        settingEvent()
 
     }
 
