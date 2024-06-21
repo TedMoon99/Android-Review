b1c6ed4412d3722b8d13f9f8eec8419b4dbd1168
kshn379 <94738162+kshn379@users.noreply.github.com>
2024-06-21 17:34:38 +0900

    Android_Review02_KSHN379

--- android_review02_kshn379/src/main/java/com/example/android_review02_kshn379/MainActivity.kt
+++ android_review02_kshn379/src/main/java/com/example/android_review02_kshn379/MainActivity.kt
@@ -15,6 +15,24 @@ import com.example.android_review02_kshn379.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    var filteredList : MutableList<Research> = mutableListOf()
+    val dataList: MutableList<Research> = mutableListOf()
+    lateinit var adapter: ResearchAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 바인딩 초기화
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // 뷰 설정
+        settingView()
+        // 이벤트 설정
+        settingEvent()
+
+    }
+
     // 이벤트 설정
     fun settingEvent() {
         binding.apply {
@@ -30,41 +48,32 @@ class MainActivity : AppCompatActivity() {
                     // datalist에 담아준다
                     dataList.add(data)
 
+                    // 어댑터 연결
+                    recyclerViewMain.adapter = ResearchAdapter(dataList)
+
                     // 어댑터에 변경을 알린다
                     recyclerViewMain.adapter?.notifyDataSetChanged()
+
+                    // 어댑터 연결
+                    adapter = ResearchAdapter(dataList)
+
+                    // 입력 요소 초기화
+                    textInputEditTextMainName.text = null
                 }
             }
 
-            textInputEditTextMainSearch.addTextChangedListener(object : TextWatcher {
-                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            textInputEditTextMainSearch.addTextChangedListener {
+                filteredList.clear()
+                filteredList.addAll(dataList.filter {
+                    it.name.contains(textInputEditTextMainSearch.text.toString(), true)
 
-                }
+                })
+                // 어댑터 연결
+                recyclerViewMain.adapter = ResearchAdapter(filteredList)
 
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                    Log.d("test1234","s : ${s}")
-                    Log.d("test1234","start : ${start}")
-                    Log.d("test1234","before : ${before}")
-                    Log.d("test1234","count : ${count}")
-                    val searchText = s.toString()
-
-                    val list = dataList.filter {
-                        it.name.contains(searchText)
-                    }
-
-                    filteredList = list
-
-                    filterData(searchText)
-                }
-
-                private fun filterData(searchText: String) {
-                    searchText
-
-
-                }
-                override fun afterTextChanged(s: Editable?) {
-
-                }
-            })
+                // 어댑터 변경 알림
+                recyclerViewMain.adapter?.notifyDataSetChanged()
+            }
         }
     }
 
@@ -81,22 +90,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    var filteredList : MutableList<Research> = mutableListOf()
-    val dataList: MutableList<Research> = mutableListOf()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // 바인딩 초기화
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        // 뷰 설정
-        settingView()
-        // 이벤트 설정
-        settingEvent()
-
-    }
 
     // 유효성 검사
     fun validateInput(): Boolean {
--- android_review02_kshn379/src/main/java/com/example/android_review02_kshn379/ResearchAdapter.kt
+++ android_review02_kshn379/src/main/java/com/example/android_review02_kshn379/ResearchAdapter.kt
@@ -6,21 +6,16 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android_review02_kshn379.databinding.ItemListBinding
 
 class ResearchAdapter(val searchList: MutableList<Research>) :
     RecyclerView.Adapter<ResearchAdapter.CustomViewHolder>() {
 
-    class CustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val name = itemView.findViewById<TextView>(R.id.rv_name)
-    }
-
     // 항목 최초 생성시 작동
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): ResearchAdapter.CustomViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+            CustomViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_list, parent, false)
-        return CustomViewHolder(view)
+        return CustomViewHolder(ItemListBinding.bind(view))
     }
 
 
@@ -32,5 +27,10 @@ class ResearchAdapter(val searchList: MutableList<Research>) :
     // 얼마나 만들 것인지
     override fun getItemCount(): Int {
         return searchList.size
+    }
+
+    class CustomViewHolder(private val binding: ItemListBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val name = binding.rvName
     }
 }
