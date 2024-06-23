9f26e7ce84c302db8e481852080a5f93068ede31
TedMoon99 <mtj1015@naver.com>
2024-06-23 13:13:42 +0900

    [Feat] CustomAdapter 화면 및 기능 생성

---
+++ android_review03_tedmoon/src/main/java/com/example/android_review03_tedmoon/adapter/CustomAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.android_review03_tedmoon.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android_review03_tedmoon.databinding.RowMainBinding
+import com.example.android_review03_tedmoon.model.ScoreInfo
+
+class CustomAdapter(val dataSet: List<ScoreInfo>): RecyclerView.Adapter<CustomAdapter.CustomViewHolder>() {
+    class CustomViewHolder(rowMainBinding: RowMainBinding): RecyclerView.ViewHolder(rowMainBinding.root){
+        val rowMainBinding: RowMainBinding
+        init {
+            this.rowMainBinding = rowMainBinding
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        val rowMainBinding = RowMainBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val viewHolder = CustomViewHolder(rowMainBinding)
+        return viewHolder
+    }
+
+    override fun getItemCount(): Int {
+        return dataSet.size
+    }
+
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+        holder.rowMainBinding.textViewRowName.text = dataSet[position].name
+        holder.rowMainBinding.textViewRowGrade.text = dataSet[position].grade.toString()
+    }
+}
