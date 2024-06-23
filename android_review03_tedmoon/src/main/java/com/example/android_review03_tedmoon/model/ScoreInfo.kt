a1280c232f6907b1876f5aa7ca25f580c30bad73
TedMoon99 <mtj1015@naver.com>
2024-06-23 13:13:18 +0900

    [Feat] data class 생성

---
+++ android_review03_tedmoon/src/main/java/com/example/android_review03_tedmoon/model/ScoreInfo.kt
@@ -0,0 +1,9 @@
+package com.example.android_review03_tedmoon.model
+
+data class ScoreInfo(
+    val name: String,
+    val grade: Int,
+    val korean: Long,
+    val english: Long,
+    val math: Long
+)
