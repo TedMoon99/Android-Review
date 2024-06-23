098645cc13a494406473ab0bc8c67d608b5c04f8
TedMoon99 <mtj1015@naver.com>
2024-06-23 13:00:37 +0900

    [Feat] enum class 생성

---
+++ android_review03_tedmoon/src/main/java/com/example/android_review03_tedmoon/utils/Tools.kt
@@ -0,0 +1,14 @@
+package com.example.android_review03_tedmoon.utils
+
+class Tools {
+    companion object{
+
+    }
+}
+
+enum class FragmentName(name: String){
+    MAIN_FRAGMENT("MainFragment"),
+    SUB_FRAGMENT1("SubFragment1"),
+    SUB_FRAGMENT2("SubFragment2"),
+
+}
