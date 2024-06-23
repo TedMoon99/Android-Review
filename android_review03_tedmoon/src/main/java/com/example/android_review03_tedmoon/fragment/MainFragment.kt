2f6e60b3862e2fc28c04409c9b252fd82c5ffe17
TedMoon99 <mtj1015@naver.com>
2024-06-23 13:00:51 +0900

    [Feat] MainFragment - 화면전환 기능 추가

--- android_review03_tedmoon/src/main/java/com/example/android_review03_tedmoon/fragment/MainFragment.kt
+++ android_review03_tedmoon/src/main/java/com/example/android_review03_tedmoon/fragment/MainFragment.kt
@@ -6,55 +6,56 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.android_review03_tedmoon.R
+import com.example.android_review03_tedmoon.databinding.FragmentMainBinding
+import com.example.android_review03_tedmoon.utils.FragmentName
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var binding: FragmentMainBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        binding = FragmentMainBinding.inflate(inflater)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // View 설정
+        settingView()
+        // Event 설정
+        settingEvent()
+    }
+
+    fun settingEvent(){
+        binding.apply {
+            // 학생 정보 입력 버튼 리스너 설정
+            buttonMainAdd.setOnClickListener {
+                parentFragmentManager.apply {
+                    beginTransaction() // 트랜잭션 생성
+                        .replace(R.id.containerMain, SubFragment1()) // containerMain SubFragment2 출력
+                        .addToBackStack(FragmentName.SUB_FRAGMENT1.name) // 백스택에 추가
+                        .commit() // 실행
                 }
             }
+
+            // 총점 및 평균
+            buttonMainTotalAverage.setOnClickListener {
+                parentFragmentManager.apply {
+                    beginTransaction() // 트랜잭션 생성
+                        .replace(R.id.containerMain, SubFragment2()) // containerMain SubFragment2 출력
+                        .addToBackStack(FragmentName.SUB_FRAGMENT2.name) // 백스택에 추가
+                        .commit() // 실행
+                }
+            }
+        }
+
+    }
+
+    fun settingView(){
+
     }
 }
