fef64f5ee3e375a31b459525d0b6914ee9115d3d
baek08102 <baek2654@gmail.com>
2024-06-24 17:36:27 +0900

    [Feat] fragment_main 화면 출력 함수 설정

--- android_review03_baek08102/src/main/java/com/example/android_review03_baek08102/MainActivity.kt
+++ android_review03_baek08102/src/main/java/com/example/android_review03_baek08102/MainActivity.kt
@@ -5,16 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.android_review03_baek08102.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 화면 출력 
+        startDevice()
+    }
+
+    // fragment_main 화면 출력 함수
+    fun startDevice() {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.main_container, MainFragment())
+            .commit()
     }
 }
