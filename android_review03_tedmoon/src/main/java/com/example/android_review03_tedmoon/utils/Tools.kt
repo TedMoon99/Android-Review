3b18b3b2808d4800ac4ad43db6696c3a6ff41554
TedMoon99 <mtj1015@naver.com>
2024-06-26 17:28:29 +0900

    [Feat] Tools.kt companion object로 Adapter에 들어갈 전체 데이터 관리

--- android_review03_tedmoon/src/main/java/com/example/android_review03_tedmoon/utils/Tools.kt
+++ android_review03_tedmoon/src/main/java/com/example/android_review03_tedmoon/utils/Tools.kt
@@ -1,7 +1,11 @@
 package com.example.android_review03_tedmoon.utils
 
+import com.example.android_review03_tedmoon.model.ScoreInfo
+
 class Tools {
     companion object{
+        // 학생 정보 객체를 담을 리스트
+        val scoreList = mutableListOf<ScoreInfo>()
 
     }
 }
