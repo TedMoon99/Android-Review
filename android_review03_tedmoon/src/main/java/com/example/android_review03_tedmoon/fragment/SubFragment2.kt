aef7cbfb2a2de09544c9216195cd6cef8c21e07a
TedMoon99 <mtj1015@naver.com>
2024-06-28 02:02:17 +0900

    [Feat] SubFragment2 - 총점 및 평균 완성

--- android_review03_tedmoon/src/main/java/com/example/android_review03_tedmoon/fragment/SubFragment2.kt
+++ android_review03_tedmoon/src/main/java/com/example/android_review03_tedmoon/fragment/SubFragment2.kt
@@ -1,60 +1,123 @@
 package com.example.android_review03_tedmoon.fragment
 
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.SystemClock
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.android_review03_tedmoon.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.android_review03_tedmoon.databinding.FragmentSub2Binding
+import com.example.android_review03_tedmoon.model.ScoreInfo
+import com.example.android_review03_tedmoon.utils.FragmentName
+import com.example.android_review03_tedmoon.utils.Tools
+import kotlin.math.roundToInt
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SubFragment2.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SubFragment2 : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentSub2Binding
+    var dataList: ArrayList<ScoreInfo>? = null
 
+    // 홀수 번째 : Total, 짝수 번째 : Average
+    val scoreArray = arrayOf(0.0, 0.0, 0.0)
+
+    /*
+    [0] : 국어 총합
+    [1] : 영어 총합
+    [2] : 수학 총합
+     */
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sub2, container, false)
+        binding = FragmentSub2Binding.inflate(inflater)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SubFragment2.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SubFragment2().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 기초 데이터 받아오기
+        initData()
+        // View 설정
+        settingView()
+        // Event 설정
+        settingEvent()
+    }
+
+    // 기초 데이터 받아오기
+    fun initData() {
+        // argumenets에서 bundle을 받아온다
+        val bundle = arguments
+        // Bundle 객체에서 key값을 이용하여 ArrayList를 뽑아서 dataList에 담아준다
+        dataList = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            // API 33이상인 경우 사용하는 코드
+            bundle?.getParcelableArrayList("dataList", ScoreInfo::class.java)
+        } else { // API 33 미만에서 사용하는 코드
+            @Suppress("DEPRECATION")
+            bundle?.getParcelableArrayList("dataList")
+        }
+        // Data 전송 test
+    }
+
+    // View 설정
+    fun settingView() {
+        // 인원 수 구하기
+        val num = dataList?.size?: 1 // dataList의 크기를 반환하고 dataList가 null이면 1을 반환한다
+        // 과목별 총점 구하기
+        settingTotalAndAverage()
+        // 전체 총점 구하기
+        val total = scoreArray.sum()
+
+        // textView에 연결
+        binding.apply {
+            // 총점 설정
+            textViewSub2KoreanTotal.text = "국어 총점 : ${scoreArray[0]}점"
+            textViewSub2EnglishTotal.text = "영어 총점 : ${scoreArray[1]}점"
+            textViewSub2MathTotal.text = "수학 총점 : ${scoreArray[2]}점"
+
+            // 평균 설정
+            textViewSub2KoreanAverage.text = "국어 평균 : ${((scoreArray[0] / num) * 100.0).roundToInt() / 100.0}점"
+            textViewSub2EnglishAverage.text = "영어 평균 : ${((scoreArray[1] / num) * 100.0).roundToInt() / 100.0}점"
+            textViewSub2MathAverage.text = "수학 평균 : ${((scoreArray[2] / num) * 100.0).roundToInt() / 100.0}점"
+
+            // 전체 총점 설정
+            textViewSub2WholeTotal.text = "전체 총점 : ${total}점"
+
+            // 전체 평균 설정
+            textViewSub2WholeAverage.text = "전체 평균 : ${((total / num) * 100.0).roundToInt() / 100.0}점"
+
+        }
+
+    }
+
+    // Event 설정
+    fun settingEvent() {
+        binding.apply {
+            buttonSub2Complete.setOnClickListener {
+                // 확인 버튼 클릭 시 MainFragment로 이동한다
+                removeFragment()
             }
+        }
+
+    }
+
+    // 총점과 평균 구하기
+    fun settingTotalAndAverage() {
+        if (dataList != null) {
+            for (item in dataList!!) {
+                scoreArray[0] += item.korean
+                scoreArray[1] += item.english
+                scoreArray[2] += item.math
+            }
+        }
+    }
+
+    // 뒤로가기
+    fun removeFragment(){
+        SystemClock.sleep(200)
+        parentFragmentManager.popBackStack(FragmentName.SUB_FRAGMENT2.name, FragmentManager.POP_BACK_STACK_INCLUSIVE)
     }
 }
