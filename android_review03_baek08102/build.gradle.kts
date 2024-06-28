d3513ee5cbc2882851586e089f8132cd76d4e0d2
baek08102 <baek2654@gmail.com>
2024-06-28 17:32:18 +0900

    [GRADLE] ViewHolder 사용 위한 종속성 추가

--- android_review03_baek08102/build.gradle.kts
+++ android_review03_baek08102/build.gradle.kts
@@ -37,6 +37,7 @@ android {
 
     buildFeatures {
         viewBinding = true
+        dataBinding = true
     }
 }
 
@@ -50,4 +51,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.2")
+    implementation("androidx.fragment:fragment-ktx:1.8.0")
 }
