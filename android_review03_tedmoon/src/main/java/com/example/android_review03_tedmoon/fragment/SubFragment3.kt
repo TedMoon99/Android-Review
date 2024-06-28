9bd96f822e1796089a59659b7bc3ada5dedffab5
TedMoon99 <mtj1015@naver.com>
2024-06-29 01:16:46 +0900

    [Feat] SubFragment3 - TextView 연결

--- android_review03_tedmoon/src/main/java/com/example/android_review03_tedmoon/fragment/SubFragment3.kt
+++ android_review03_tedmoon/src/main/java/com/example/android_review03_tedmoon/fragment/SubFragment3.kt
@@ -14,6 +14,7 @@ import com.example.android_review03_tedmoon.model.ScoreInfo
 class SubFragment3 : Fragment() {
 
     private lateinit var binding: FragmentSub3Binding
+    var data: ScoreInfo? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -38,7 +39,7 @@ class SubFragment3 : Fragment() {
         // argumenets에서 bundle을 받아온다
         val bundle = arguments
         // Bundle 객체에서 key값을 이용하여 ArrayList를 뽑아서 dataList에 담아준다
-        val data = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        data = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             // API 33이상인 경우 사용하는 코드
             bundle?.getParcelable("positionData", ScoreInfo::class.java)
         } else { // API 33 미만에서 사용하는 코드
@@ -52,6 +53,22 @@ class SubFragment3 : Fragment() {
 
     // View 설정
     fun settingView(){
+        binding.apply {
+            // 이름 설정
+            textViewSub3Name.text = "이름 : ${data?.name}"
+            // 학년 설정
+            textViewSub3Grade.text = "학년 : ${data?.grade}학년"
+            // 국어
+            textViewSub3Korean.text = "국어 점수 : ${data?.korean}점"
+            // 영어
+            textViewSub3English.text = "영어 점수 : ${data?.english}점"
+            // 수학
+            textViewSub3Math.text = "수학 점수 : ${data?.math}점"
+            // 총점
+            textViewSub3Total.text = "총점 test"
+            // 평균
+            textViewSub3Average.text = "평균 test"
+        }
 
     }
     // Event 설정
