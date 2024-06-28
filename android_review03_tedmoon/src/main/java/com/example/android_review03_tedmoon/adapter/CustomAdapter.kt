bed45f0afa91c76c8af31a890a94ed0fc33e7b08
TedMoon99 <mtj1015@naver.com>
2024-06-29 00:57:18 +0900

    [Feat] CustomAdapter - 1. 어댑터 클릭 시 화면전환 작동 확인 2. 화면전환 시 데이터 전송 확인

--- android_review03_tedmoon/src/main/java/com/example/android_review03_tedmoon/adapter/CustomAdapter.kt
+++ android_review03_tedmoon/src/main/java/com/example/android_review03_tedmoon/adapter/CustomAdapter.kt
@@ -1,17 +1,34 @@
 package com.example.android_review03_tedmoon.adapter
 
+import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android_review03_tedmoon.R
 import com.example.android_review03_tedmoon.databinding.RowMainBinding
+import com.example.android_review03_tedmoon.fragment.SubFragment3
 import com.example.android_review03_tedmoon.model.ScoreInfo
+import com.example.android_review03_tedmoon.utils.FragmentName
 
-class CustomAdapter(val dataSet: ArrayList<ScoreInfo>): RecyclerView.Adapter<CustomAdapter.CustomViewHolder>() {
-    class CustomViewHolder(rowMainBinding: RowMainBinding): RecyclerView.ViewHolder(rowMainBinding.root){
-        val rowMainBinding: RowMainBinding
-        init {
-            this.rowMainBinding = rowMainBinding
+class CustomAdapter(private val dataSet: ArrayList<ScoreInfo>, private val manager: FragmentManager): RecyclerView.Adapter<CustomAdapter.CustomViewHolder>() {
+    class CustomViewHolder(val rowMainBinding: RowMainBinding): RecyclerView.ViewHolder(rowMainBinding.root){
+
+        // 클릭 리스너 설정
+        fun clickListener(scoreInfo: ScoreInfo, manager: FragmentManager){
+            // Fragment 생성
+            val subFragment3 = SubFragment3().apply {
+                arguments = Bundle().apply {
+                    putParcelable("positionData", scoreInfo)
+                }
+            }
+            // 화면 이동
+            manager
+                .beginTransaction() // 트랜잭션 생성
+                .replace(R.id.containerMain, subFragment3) // 화면 이동
+                .addToBackStack(FragmentName.SUB_FRAGMENT3.name) // 백스택 추가
+                .commit() // 실행
         }
     }
 
@@ -28,5 +45,12 @@ class CustomAdapter(val dataSet: ArrayList<ScoreInfo>): RecyclerView.Adapter<Cus
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
         holder.rowMainBinding.textViewRowName.text = "이름 : ${dataSet[position].name}"
         holder.rowMainBinding.textViewRowGrade.text = "학년 : ${dataSet[position].grade}학년"
+
+        holder.itemView.setOnClickListener {
+            val data = dataSet[position]
+            Log.d("test1234", "데이터 전송 확인 : $data")
+            // 클릭 리스너
+            holder.clickListener(data, manager)
+        }
     }
 }
