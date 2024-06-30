07b969c718d6cbe844271022ff32dd0f2649fd67
kshn379 <94738162+kshn379@users.noreply.github.com>
2024-06-30 14:13:41 +0900

    [Fix] Student - 출력할 데이터 추가

--- android_review03_kshn379/src/main/java/com/example/android_review03_kshn379/Student.kt
+++ android_review03_kshn379/src/main/java/com/example/android_review03_kshn379/Student.kt
@@ -3,18 +3,28 @@ package com.example.android_review03_kshn379
 import android.os.Parcel
 import android.os.Parcelable
 
-data class Student ( val name : String, val grade: Int )
+data class Student ( val name : String, val grade: Int, val kor: Double, val eng: Double, val math: Double, val total: Double, val average: Double)
     : Parcelable {
         constructor(
             parce1: Parcel
         ) : this(
             parce1.readString().toString(),
-            parce1.readInt()
+            parce1.readInt(),
+            parce1.readDouble(),
+            parce1.readDouble(),
+            parce1.readDouble(),
+            parce1.readDouble(),
+            parce1.readDouble()
         )
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeString(name)
         parcel.writeInt(grade)
+        parcel.writeDouble(kor)
+        parcel.writeDouble(eng)
+        parcel.writeDouble(math)
+        parcel.writeDouble(total)
+        parcel.writeDouble(average)
     }
 
     override fun describeContents(): Int {
