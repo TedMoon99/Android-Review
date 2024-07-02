f427a84d5a6b00764f807721c31033100b9d2f1c
baek08102 <baek2654@gmail.com>
2024-07-02 16:53:53 +0900

    [Feat] InformFragment - 화면 출력 작업 구현

--- android_review03_baek08102/src/main/java/com/example/android_review03_baek08102/InformFragment.kt
+++ android_review03_baek08102/src/main/java/com/example/android_review03_baek08102/InformFragment.kt
@@ -5,55 +5,76 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.activityViewModels
+import com.example.android_review03_baek08102.databinding.FragmentInformBinding
+import com.example.android_review03_baek08102.databinding.FragmentInputBinding
+import kotlin.math.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [InformFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class InformFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentInformBinding
+    private val viewModel: CustomViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inform2, container, false)
+
+        binding = FragmentInformBinding.inflate(inflater)
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment InformFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            InformFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    // 일반 함수 선언
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        settingView()
+        settingEvent()
+    }
+
+    // 이벤트 설정 함수
+    fun settingEvent() {
+        binding.informButtonConfirm.setOnClickListener { // 버튼 클릭 시
+            parentFragmentManager.popBackStack(                  // backstack 제거,
+                FragmentName.INFORM_FRAGMENT.name,               // 현재 프래그먼트
+                FragmentManager.POP_BACK_STACK_INCLUSIVE
+            )
+        }
+    }
+
+    fun settingView() {
+        binding.apply {
+
+            settingInform(viewModel.adapterPosition.value)
+
+        }
+    }
+
+    fun settingInform(position: Int?) {
+        binding.apply {
+            // viewModel 리스트 받아와서
+            val studentInform: List<StudentData>? = viewModel.studentList.value
+
+            // position이 null이 아닐 시
+            if (position != null) {
+                // adapter에서 viewModel에 넘겨준 position 가져와서
+                studentInform?.get(position)?.apply {
+
+                    // viewModel에 선언된 studentList의 position 번째 index의 항목 적용, 출력
+                    val studentTotal = koreanScore + englishScore + mathScore
+                    val studentAverage = studentTotal / 3
+
+                    informTextViewName.text = name
+                    informTextViewGrade.text = "${grade} 학년"
+                    informTextViewKoreanScore.text = "국어 점수 : ${koreanScore} 점"
+                    informTextViewEnglishScore.text = "영어 점수 : ${englishScore} 점"
+                    informTextViewMathScore.text = "수학 점수 : ${mathScore} 점"
+                    informTextViewTotal.text = "총점 : ${studentTotal} 점"
+                    informTextViewAverage.text = "평균 : ${round(studentAverage * 10) / 10} 점"
                 }
             }
+        }
     }
 }
