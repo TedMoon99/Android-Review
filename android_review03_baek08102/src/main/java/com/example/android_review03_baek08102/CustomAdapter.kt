9ff5466433e95658e0b003066e0f27fe1662372f
baek08102 <baek2654@gmail.com>
2024-07-02 16:58:32 +0900

    [Feat] CustomAdapter - 버튼 클릭 이벤트 추가

--- android_review03_baek08102/src/main/java/com/example/android_review03_baek08102/CustomAdapter.kt
+++ android_review03_baek08102/src/main/java/com/example/android_review03_baek08102/CustomAdapter.kt
@@ -1,18 +1,29 @@
 package com.example.android_review03_baek08102
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android_review03_baek08102.databinding.ActivityMainBinding
 
 class CustomAdapter(
     private val studentData: List<StudentData>,
+    private val viewModel: CustomViewModel,
+    private val mainActivity: MainActivity
 ) : RecyclerView.Adapter<CustomAdapter.CustomViewHolder>() {
-
     class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val name: TextView
         val grade: TextView
+
+
+        // 버튼 클릭 시 동작 함수
+        fun clickListener(mainActivity: MainActivity) {
+
+            mainActivity.switchFragment(FragmentName.INFORM_FRAGMENT) // 프래그먼트 전환
+        }
 
         init {
             name = view.findViewById(R.id.row_textView_name)
@@ -32,6 +43,12 @@ class CustomAdapter(
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
         holder.name.text = studentData[position].name
-        holder.grade.text = studentData[position].grade.toString()
+        holder.grade.text = "${studentData[position].grade} 학년"
+        // 버튼 클릭 시
+        holder.itemView.setOnClickListener {
+            // clickListener 함수 실행
+            holder.clickListener(mainActivity)
+            viewModel.getPosition(position)
+        }
     }
 }
