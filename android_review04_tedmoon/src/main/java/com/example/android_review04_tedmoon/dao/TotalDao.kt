25df78d5132843d83cc6200292b1ea3757448e9f
TedMoon99 <mtj1015@naver.com>
2024-07-08 16:16:03 +0900

    [Feat] TotalDao - 총점과 평균 데이터를 관리할 DAO 생성

--- android_review04_tedmoon/src/main/java/com/example/android_review04_tedmoon/dao/TotalDao.kt
+++ android_review04_tedmoon/src/main/java/com/example/android_review04_tedmoon/dao/TotalDao.kt
@@ -1,63 +1,59 @@
-//package com.example.android_review04_tedmoon.dao
-//
-//import com.google.firebase.Firebase
-//import com.google.firebase.firestore.firestore
-//import kotlinx.coroutines.CoroutineScope
-//import kotlinx.coroutines.Dispatchers
-//import kotlinx.coroutines.launch
-//import kotlinx.coroutines.tasks.await
-//
-//class TotalDao {
-//    // 학생번호 시퀀스를 불러옴
-//    suspend fun gettingSequence(): Int{
-//        var sequence = -1
-//        val job1 = CoroutineScope(Dispatchers.IO).launch {
-//            // 컬랙션 접근 객체를 가져온다
-//            val collectionReference = Firebase.firestore.collection("Sequence")
-//
-//            val documentReference = collectionReference.document("StudentSequence")
-//
-//            val sequenceSnapShot = documentReference.get().await()
-//
-//            sequence = sequenceSnapShot.getLong("value")?.toInt()!!
-//        }
-//        job1.join()
-//
-//        return sequence
-//    }
-//
-//    // 학생번호 시퀀스 업데이트
-//    suspend fun updateSqeuence(studentSequence: Int){
-//        val job1 = CoroutineScope(Dispatchers.IO).launch {
-//            // 컬랙션 접근 객체를 가져온다
-//            val collectionReference = Firebase.firestore.collection("Sequence")
-//
-//            val documentReference = collectionReference.document("StudentSequence")
-//
-//            val map = mutableMapOf<String, Long>()
-//            map["value"] = studentSequence.toLong()
-//            // 저장한다
-//            documentReference.set(map)
-//        }
-//
-//        job1.join()
-//    }
-//
-//    // 총점 및 평균 데이터를 저장한다
-//    suspend fun saveTotal(totalData: Map<String, Long>){
-//        val job1 = CoroutineScope(Dispatchers.IO).launch {
-//            // 컬랙션 접근 객체를 가져온다
-//            val collectionReference = Firebase.firestore.collection("TotalData")
-//            // 컬랙션에 문서를 추가한다
-//            // 문서를 추가할 때 객체나 맵을 지정한다
-//            // 추가된 문서 내부의 필드는 객체가 가진 프로퍼티의 이름이나 맵에 있는 데이터의 이름과 동일하게 결정된다
-//            collectionReference.add(totalData)
-//        }
-//        job1.join()
-//    }
-//
-//    // 총점 및 평균 데이터를 업데이트 한다
-////    suspend fun updateTotal(totalData){
-////
-////    }
-//}
+package com.example.android_review04_tedmoon.dao
+
+import android.util.Log
+import com.example.android_review04_tedmoon.model.ScoreInfo
+import com.example.android_review04_tedmoon.model.TotalInfo
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.ktx.firestore
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+
+class TotalDao {
+    companion object{
+
+        // 총점과 평균을 불러온다(국어, 영어, 수학, 전체)
+        suspend fun gettingTotalData(): TotalInfo {
+            var totalInfo: TotalInfo = TotalInfo()
+            try {
+                val querySnapshot = Firebase.firestore.collection("TotalData").get().await()
+                querySnapshot.forEach {
+                    // ScoreInfo 객채에 담아준다
+                    totalInfo = it.toObject(TotalInfo::class.java)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            return totalInfo
+        }
+
+        // 총점과 평균을 업데이트한다
+        suspend fun settingTotalData(totalInfo: TotalInfo){
+            try {
+                // 컬랙션에 접근할 수 있는 객체를 가져온다
+                val collectionReference = Firebase.firestore.collection("TotalData").get().await()
+
+                // 저장할 데이터를 담을 HashMap을 만들어준다
+                val map = mutableMapOf<String, Any?>()
+                map["totalIdx"] = totalInfo.totalIdx
+                map["koreanTotal"] = totalInfo.koreanTotal
+                map["englishTotal"] = totalInfo.englishTotal
+                map["mathTotal"] = totalInfo.mathTotal
+                map["koreanAverage"] = totalInfo.koreanAverage
+                map["englishAverage"] = totalInfo.englishAverage
+                map["mathAverage"] = totalInfo.mathAverage
+                map["wholeTotal"] = totalInfo.wholeTotal
+                map["wholeAverage"] = totalInfo.wholeAverage
+
+                // 저장한다
+                // 가져온 문서 중에서 첫 번째 문서에 접근하여 데이터를 수정한다
+                collectionReference.documents[0].reference.update(map)
+
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
