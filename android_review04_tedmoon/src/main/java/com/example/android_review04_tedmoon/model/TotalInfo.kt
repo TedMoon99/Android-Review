504838f7acc3e4daf997328ca9f577f2d98c7503
TedMoon99 <mtj1015@naver.com>
2024-07-08 16:16:50 +0900

    [Feat] TotalInfo - data class 생성

---
+++ android_review04_tedmoon/src/main/java/com/example/android_review04_tedmoon/model/TotalInfo.kt
@@ -0,0 +1,14 @@
+package com.example.android_review04_tedmoon.model
+
+data class TotalInfo(
+    val totalIdx: Int,
+    val koreanTotal: Double,
+    val englishTotal: Double,
+    val mathTotal: Double,
+    val koreanAverage: Double,
+    val englishAverage: Double,
+    val mathAverage: Double,
+    val wholeTotal: Double,
+    val wholeAverage: Double){
+ constructor(): this(0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+}
