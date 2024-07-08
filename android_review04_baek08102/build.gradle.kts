53c7ec400ac7238ac9499d9e62fe0f32e07d319c
baek08102 <baek2654@gmail.com>
2024-07-08 19:38:46 +0900

    [Gradle] build.gradle.kts - setFragmentResult 사용 위한 종속성 추가

--- android_review04_baek08102/build.gradle.kts
+++ android_review04_baek08102/build.gradle.kts
@@ -40,6 +40,8 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.fragment.ktx.v181)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
