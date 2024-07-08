578b02177d549a32c692b2910ae56ff9d7610317
TedMoon99 <mtj1015@naver.com>
2024-07-08 15:35:12 +0900

    [Feat] MainFragment - DB에서 데이터 불러와서 Adapter에게 연결

--- android_review04_tedmoon/src/main/java/com/example/android_review04_tedmoon/fragment/MainFragment.kt
+++ android_review04_tedmoon/src/main/java/com/example/android_review04_tedmoon/fragment/MainFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.android_review04_tedmoon.R
 import com.example.android_review04_tedmoon.adapter.CustomAdapter
@@ -19,12 +21,13 @@ import com.google.android.material.divider.MaterialDividerItemDecoration
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainFragment : Fragment() {
 
     private lateinit var binding: FragmentMainBinding
     private lateinit var viewModel: MainViewModel
-    var dataList: ArrayList<ScoreInfo> = arrayListOf()
+    val dataList: ArrayList<ScoreInfo> = arrayListOf()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -41,6 +44,8 @@ class MainFragment : Fragment() {
         settingView()
         // Event 설정
         settingEvent()
+        // 서버로부터 데이터를 가져와 리사이클러뷰를 갱신한다
+        gettingAdapterData()
     }
 
     // View 설정
@@ -54,14 +59,14 @@ class MainFragment : Fragment() {
             // RecyclerView
             recyclerViewMain.apply {
                 // 구분선 생성
-                val deco = MaterialDividerItemDecoration(context, LinearLayoutManager.VERTICAL)
-//                // 데이터 불러오기
-                gettingAdapterData()
-
+                val deco = MaterialDividerItemDecoration(context, LinearLayoutManager.VERTICAL).apply {
+                    // 마지막 항목 구분선 제거
+                    isLastItemDecorated = false
+                }
                 // 어댑터 연결
                 adapter = CustomAdapter(dataList)
                 // 레이아웃 매니저 적용
-                layoutManager = LinearLayoutManager(this@MainFragment.context)
+                layoutManager = LinearLayoutManager(context)
                 // 구분선 적용
                 addItemDecoration(deco)
             }
@@ -70,14 +75,19 @@ class MainFragment : Fragment() {
 
     // DB에서 모든 정보를 가져온다
     fun gettingAdapterData() {
-        CoroutineScope(Dispatchers.Main).launch {
-            // DB에서 모든 정보를 가져온다
-            val data = ScoreDao.getAllData()
-            Log.d("test1234", "DB에서 불러온 데이터 : ${data}")
-            if (data.isNotEmpty()) {
-                dataList = data
-            } else {
-                Log.d("test1234", "DB에서 불러온 데이터가 비어있습니다")
+        viewLifecycleOwner.lifecycleScope.launch {
+            try {
+                // 백그라운드 스레드에서 데이터를 가져온다
+                val data = withContext(Dispatchers.IO){ ScoreDao.getAllData() }
+
+                dataList.clear()
+                dataList.addAll(data)
+
+                // 어댑터에게 데이터 변경 알림
+                binding.recyclerViewMain.adapter?.notifyDataSetChanged()
+
+            } catch (e: Exception){
+                e.printStackTrace()
             }
         }
     }
@@ -85,8 +95,10 @@ class MainFragment : Fragment() {
     // Event 설정
     private fun settingEvent() {
         binding.apply {
+
             // Toolbar 리스너
             toolbarMain.apply {
+
                 // menu Item 리스너 설정
                 setOnMenuItemClickListener {
                     when (it.itemId) {
@@ -102,6 +114,13 @@ class MainFragment : Fragment() {
                         }
                     }
                     true
+                }
+
+                // 새로고침
+                setNavigationOnClickListener {
+                    Toast.makeText(context, "새로고침", Toast.LENGTH_SHORT).show()
+                    // data 변경 알림
+                    gettingAdapterData()
                 }
             }
         }
