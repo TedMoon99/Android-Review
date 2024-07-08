7919232af6a06c441b37ba95df50199c63d44882
TedMoon99 <mtj1015@naver.com>
2024-07-08 15:37:30 +0900

    [Feat] ScoreInfo - Firebase에서 데이터를 불러올 때 사용할 constuctor 추가

--- android_review04_tedmoon/src/main/java/com/example/android_review04_tedmoon/model/ScoreInfo.kt
+++ android_review04_tedmoon/src/main/java/com/example/android_review04_tedmoon/model/ScoreInfo.kt
@@ -7,4 +7,9 @@ data class ScoreInfo(
     val korean: Double,
     val english: Double,
     val math: Double
-)
+){
+    // 매개변수가 없는 생성자
+    // firestore를 사용할 때 데이터를 담을 클래스를 지정하게 되면(ScoreInfo::class.java)
+    // 매개변수가 없는 생성자를 사용하여 객체를 생성해주기 때문에 만들어주어야 한다
+    constructor(): this(0, "", 0, 0.0, 0.0, 0.0)
+}
