fb7452ac27fe4ed2f0ae3f8783b44b76f7fb0b68
baek08102 <baek2654@gmail.com>
2024-07-10 18:28:23 +0900

    [Feat] MainActivity - View에 system bar 만큼 padding 추가하여 toolbar와 겹치던 현상 수정

--- android_review04_baek08102/src/main/java/com/example/android_review04_baek08102/MainActivity.kt
+++ android_review04_baek08102/src/main/java/com/example/android_review04_baek08102/MainActivity.kt
@@ -16,6 +16,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
         startDevice()
     }
 
