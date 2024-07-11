eff9b7262bb6b692f480c37da805d39c2be19aa7
TedMoon99 <mtj1015@naver.com>
2024-07-11 11:44:54 +0900

    [Feat] FragmentName enum class 생성

---
+++ android_review05_tedmoon/src/main/java/com/example/android_review05_tedmoon/utils/Tools.kt
@@ -0,0 +1,11 @@
+package com.example.android_review05_tedmoon.utils
+
+class Tools {
+    companion object{
+
+    }
+}
+
+enum class FragmentName(var str: String){
+    ADD_FRAGMENT("AddFragment"),
+}
