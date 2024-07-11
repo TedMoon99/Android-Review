3464d02c46973975331ed2c94d6804c74acb5c56
TedMoon99 <mtj1015@naver.com>
2024-07-11 18:40:27 +0900

    [Feat] CustomAdapter.kt 생성

---
+++ android_review05_tedmoon/src/main/java/com/example/android_review05_tedmoon/adapter/CustomAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.android_review05_tedmoon.adapter
+
+import android.view.LayoutInflater
+import android.view.MenuInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android_review05_tedmoon.R
+import com.example.android_review05_tedmoon.databinding.RowMainBinding
+import com.example.android_review05_tedmoon.model.ScoreInfo
+
+class CustomAdapter(val dataSet: ArrayList<ScoreInfo>, val menuInflater: MenuInflater): RecyclerView.Adapter<CustomAdapter.CustomViewHolder>() {
+
+    class CustomViewHolder(rowMainBinding: RowMainBinding, menuInflater: MenuInflater): RecyclerView.ViewHolder(rowMainBinding.root){
+        val rowMainBinding: RowMainBinding
+        init {
+            this.rowMainBinding = rowMainBinding
+
+            // 클릭 영역 자체를 항목의 전체가 되도록 설정한다
+            rowMainBinding.root.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, // 가로 전체
+                ViewGroup.LayoutParams.WRAP_CONTENT // 세로 전체
+            )
+
+            // RecyclerView의 항목에 컨텍스트 메뉴를 설정해준다
+            rowMainBinding.root.setOnCreateContextMenuListener { menu, v, menuInfo ->
+                // 메뉴의 헤더
+                menu?.setHeaderTitle("삭제하시겠습니까?")
+                // 메뉴를 구성한다
+                menuInflater.inflate(R.menu.menu_customadapter_contextmenu, menu)
+                // 각 메뉴 아이템을 추출하여 리스너를 설정해준다
+                menu?.findItem(R.id.menuItem_adapter_delete)?.setOnMenuItemClickListener {
+                    // DB 삭제 구현
+                    // ### DB 연동하고 나면 곧 합니다~ ###
+                    true
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        return CustomViewHolder(RowMainBinding.inflate(LayoutInflater.from(parent.context)), menuInflater)
+    }
+
+    override fun getItemCount(): Int {
+        return dataSet.size
+    }
+
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+        holder.rowMainBinding.textViewRowMainName.text = "이름 : ${dataSet[position].name}"
+        holder.rowMainBinding.textViewRowMainGrade.text = "학년 : ${dataSet[position].grade}학년"
+    }
+}
