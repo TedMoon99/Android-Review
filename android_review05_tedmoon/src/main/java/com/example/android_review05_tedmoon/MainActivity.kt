8deb62798da08e193a3b4416ccfe7504cb37e8f9
TedMoon99 <mtj1015@naver.com>
2024-07-11 11:44:30 +0900

    [Style] package 추가로 인한 경로 재설정

--- android_review05_tedmoon/src/main/java/com/example/android_review05_tedmoon/MainActivity.kt
+++ android_review05_tedmoon/src/main/java/com/example/android_review05_tedmoon/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.android_review05_tedmoon.databinding.ActivityMainBinding
+import com.example.android_review05_tedmoon.fragment.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
