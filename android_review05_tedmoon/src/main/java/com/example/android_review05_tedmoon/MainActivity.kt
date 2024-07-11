a1fd13426b2d06bf47ea5b73792cf526ea6df24f
TedMoon99 <mtj1015@naver.com>
2024-07-11 11:30:43 +0900

    [Feat | UI] MainActivity - FragmentContainerView 적용

--- android_review05_tedmoon/src/main/java/com/example/android_review05_tedmoon/MainActivity.kt
+++ android_review05_tedmoon/src/main/java/com/example/android_review05_tedmoon/MainActivity.kt
@@ -5,16 +5,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.android_review05_tedmoon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // containerMain에 보여지는 화면 -> MainFragment로 설정
+        supportFragmentManager.beginTransaction().replace(R.id.containerMain, MainFragment()).addToBackStack(null).commit()
     }
 }
