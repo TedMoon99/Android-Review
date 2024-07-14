9d2fc1ef2a8008eaadbddb8cd00cd7ca98ef7a37
kshn379 <94738162+kshn379@users.noreply.github.com>
2024-07-14 16:25:02 +0900

    [Feat] StudentAdpater - ViewHolder(Recycler View Item) 클릭 시 작동 구현

--- android_review04_kshn3792/src/main/java/com/example/android_review04_kshn3792/StudentAdpater.kt
+++ android_review04_kshn3792/src/main/java/com/example/android_review04_kshn3792/StudentAdpater.kt
@@ -1,50 +1,52 @@
 package com.example.android_review04_kshn3792
 
+import android.os.Bundle
+import android.text.TextUtils.replace
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.replace
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android_review04_kshn3792.databinding.ItemsBinding
 
-class StudentAdpater (
-    private val dataSet : ArrayList<StudentInfo>, val manager: FragmentManager): RecyclerView.Adapter<StudentAdpater.DataViewHolder>() {
-        // ViewHolder
-    class DataViewHolder (itemsBinding: ItemsBinding, manager: FragmentManager):RecyclerView.ViewHolder(itemsBinding.root){
-        val itemsBinding: ItemsBinding
+class StudentAdpater(
+    private val dataSet: ArrayList<StudentInfo>, val manager: FragmentManager
+) : RecyclerView.Adapter<StudentAdpater.DataViewHolder>() {
+    // ViewHolder
+    class DataViewHolder(val itemsBinding: ItemsBinding) :
+        RecyclerView.ViewHolder(itemsBinding.root) {
 
-        init {
-            this.itemsBinding = itemsBinding
-
-            // 항목의 클릭 영역을 항목 자체로 설정한다
-            itemsBinding.root.layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT, // 가로 전체 너비 클릭
-                ViewGroup.LayoutParams.WRAP_CONTENT // 세로 맞춤 높이 클릭
-            )
-
-            // 항목을 Long 클릭 시 메뉴를 띄워 준다
-            itemsBinding.root.setOnCreateContextMenuListener { menu, v, menuInfo ->
-
+        // ViewHolder 클릭 시 작동
+        fun onClick(manager: FragmentManager, position: Int) {
+            val infoFragment = InfoFragment().apply {
+                val data = Bundle().apply {
+                    putInt("position",position)
+                }
+                arguments = data
             }
-
+            // 화면 전환 코드 작성
+            manager
+                .beginTransaction()
+                .replace(R.id.containerMain, infoFragment)
+                .addToBackStack(FragmentName.INFO_FRAGMENT.str)
+                .commit()
         }
-            fun onClick(manager: FragmentManager) {
-                // 화면 전환 코드 작성
-                manager
-                    .beginTransaction()
-                    .replace(R.id.containerMain, InfoFragment())
-                    .addToBackStack(FragmentName.INFO_FRAGMENT.str)
-                    .commit()
-            }
     }
 
+    // ViewHolder 최초 생성 시 작동
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataViewHolder {
-        return DataViewHolder(ItemsBinding.inflate(LayoutInflater.from(parent.context)), manager)
+        // binding 적용
+        val itemsBinding = ItemsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return DataViewHolder(itemsBinding)
+
     }
 
+    // 몇 개의 Adapter를 만들어 줄 건가
     override fun getItemCount(): Int {
         return dataSet.size
     }
 
+    // ViewHolder 재사용 시 작동
     override fun onBindViewHolder(holder: DataViewHolder, position: Int) {
         holder.itemsBinding.textViewItemsName.text = "이름 : ${dataSet[position].studentName}"
         holder.itemsBinding.textViewItemsGrade.text = "학년 : ${dataSet[position].studentGrade} 학년"
@@ -52,7 +54,7 @@ class StudentAdpater (
         // 클릭 리스너 설정
         holder.itemView.setOnClickListener {
             // 화면 전환
-            holder.onClick(manager)
+            holder.onClick(manager, position)
         }
     }
 
