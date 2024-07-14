7715b77d76f5dd52540399dc314d96a96ec60f83
kshn379 <94738162+kshn379@users.noreply.github.com>
2024-07-14 16:14:29 +0900

    [Feat] AddDao - Firebase 연동 및 Firebase 컬렉션, 문서 data 저장 및 불러오기 설정

--- android_review04_kshn3792/src/main/java/com/example/android_review04_kshn3792/AddDao.kt
+++ android_review04_kshn3792/src/main/java/com/example/android_review04_kshn3792/AddDao.kt
@@ -28,57 +28,61 @@ class AddDao {
                 Log.e("Dao", "Sequence 조회 실패 : ${e.message}")
             }
         }
-    }
 
-    // 학생 번호 시퀀스 값을 업데이트 한다
-    suspend fun updateSequence(studentSequence: Int) {
-        try {
-            val collectionReference = Firebase.firestore.collection("Sequence")
-            val documentReference = collectionReference.document("StudentSequence")
-            val map = mutableMapOf<String, Long>()
-            map["value"] = studentSequence.toLong()
-            documentReference.set(map).await()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
 
-    // 학생 정보를 저장한다
-    suspend fun saveStudentData(studentInfo: StudentInfo) {
-        try {
-            val collectionReference = Firebase.firestore.collection("ScoreData")
-            collectionReference.add(studentInfo).await()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-
-    // 학생 번호를 통해 학생 정보를 가져와 변환한다
-    suspend fun gettingStudentInfoByStudentIdx(studentIdx: Int): StudentInfo? {
-        return try {
-            val collectionReference = Firebase.firestore.collection("ScoreData")
-            val querySnapshot = collectionReference.whereEqualTo("studentIdx", studentIdx).get().await()
-            querySnapshot.documents.firstOrNull()?.toObject(StudentInfo::class.java)
-        } catch (e: Exception) {
-            e.printStackTrace()
-            null
-        }
-    }
-
-    // 모든 사용자의 정보를 가져온다
-    suspend fun getAllData(): ArrayList<StudentInfo> {
-        val scoreData = arrayListOf<StudentInfo>()
-        try {
-            val querySnapshot = Firebase.firestore.collection("ScoreData").get().await()
-            querySnapshot.forEach {
-                // StudentInfo 객체에 담아준다
-                val studentInfo = it.toObject(StudentInfo::class.java)
-                // 리스트에 담아준다
-                scoreData.add(studentInfo)
+        // 학생 번호 시퀀스 값을 업데이트 한다
+        suspend fun updateSequence(studentSequence: Int) {
+            try {
+                val collectionReference = Firebase.firestore.collection("Sequence")
+                val documentReference = collectionReference.document("StudentSequence")
+                val map = mutableMapOf<String, Long>()
+                map["value"] = studentSequence.toLong()
+                documentReference.set(map).await()
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
-        } catch (e: Exception) {
-            Log.e("getAllData", "데이터 조회 실패: ${e.message}")
         }
-        return scoreData
+
+        // 학생 정보를 저장한다
+        suspend fun saveStudentData(studentInfo: StudentInfo) {
+            try {
+                val collectionReference = Firebase.firestore.collection("ScoreData")
+                collectionReference.add(studentInfo).await()
+                Log.d("AddDao", "학생 정보 저장 성공: $studentInfo")
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        // 학생 번호를 통해 학생 정보를 가져와 변환한다
+        suspend fun gettingStudentInfoByStudentIdx(studentIdx: Int): StudentInfo? {
+            return try {
+                val collectionReference = Firebase.firestore.collection("ScoreData")
+                val querySnapshot =
+                    collectionReference.whereEqualTo("studentIdx", studentIdx).get().await()
+                querySnapshot.documents.firstOrNull()?.toObject(StudentInfo::class.java)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Log.e("AddDao", "학생 정보 조회 실패: ${e.message}")
+                null
+            }
+        }
+
+        // 모든 사용자의 정보를 가져온다
+        suspend fun getAllData(): ArrayList<StudentInfo> {
+            val scoreData = arrayListOf<StudentInfo>()
+            try {
+                val querySnapshot = Firebase.firestore.collection("ScoreData").get().await()
+                querySnapshot.forEach {
+                    // StudentInfo 객체에 담아준다
+                    val studentInfo = it.toObject(StudentInfo::class.java)
+                    // 리스트에 담아준다
+                    scoreData.add(studentInfo)
+                }
+            } catch (e: Exception) {
+                Log.e("getAllData", "데이터 조회 실패: ${e.message}")
+            }
+            return scoreData
+        }
     }
 }
