eb5b9c8095e3402c517f1040dab86bce346c19bb
kshn379 <94738162+kshn379@users.noreply.github.com>
2024-07-14 16:27:10 +0900

    [Feat] StudentInfo - 필드 객체 번호 생성

--- android_review04_kshn3792/src/main/java/com/example/android_review04_kshn3792/StudentInfo.kt
+++ android_review04_kshn3792/src/main/java/com/example/android_review04_kshn3792/StudentInfo.kt
@@ -1,9 +1,13 @@
 package com.example.android_review04_kshn3792
 
 data class StudentInfo(
+    val studentIdx: Int,
     val studentName: String,
     val studentGrade: Int,
     val studentKor: Double,
     val studentEng: Double,
     val studentMath: Double
-)
+){
+    // 매개변수가 없는 생성자를 사용하여 객체를 생성해주기 때문에 만들어 주어야 한다
+    constructor() : this(0,"",0,0.0,0.0,0.0)
+}
