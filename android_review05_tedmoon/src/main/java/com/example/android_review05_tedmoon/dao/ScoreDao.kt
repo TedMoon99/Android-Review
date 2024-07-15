53a08cf4e81862ba5d4bce024b831c92ce6c66c3
TedMoon99 <mtj1015@naver.com>
2024-07-15 16:42:31 +0900

    [Feat] ScoreDao 1. Sequence 조회 2. Sequence 업데이트 3. 학생정보 저장 4. 학생정보 조회

--- android_review05_tedmoon/src/main/java/com/example/android_review05_tedmoon/dao/ScoreDao.kt
+++ android_review05_tedmoon/src/main/java/com/example/android_review05_tedmoon/dao/ScoreDao.kt
@@ -1,7 +1,73 @@
 package com.example.android_review05_tedmoon.dao
 
+import android.util.Log
+import com.example.android_review05_tedmoon.model.ScoreInfo
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.toObject
+import kotlinx.coroutines.tasks.await
+
 class ScoreDao {
     companion object {
+        // Firebase에서 Seuqence 정보를 받아온다
+        suspend fun getSequence(): Int{
+            return try {
+                // 컬랙션에 접근 가능한 객체 생성
+                val collectionReference = Firebase.firestore.collection("Sequence")
+                // 문서에 접근 가능한 객체 생성
+                val documentReference = collectionReference.document("StudentSequence").get().await()
+                // 필드에 접근하여 Sequence를 가져온다
+                val snapShot = documentReference.getLong("value")?.toInt() ?: -1
+                // Sequence 반환
+                snapShot
+            } catch (e: Exception){
+                Log.e("ScoreDao", "Sequence 조회 실패 : ${e.message}")
+            }
+        }
 
+        // Firebase에 Sequence 정보를 업데이트한다
+        suspend fun updateSequence(sequence: Int){
+            try {
+                // 컬랙션에 접근 가능한 객체 생성
+                val collectionReference = Firebase.firestore.collection("Sequence")
+                // 문서에 접근 가능한 객체 생성
+                val documentReference = collectionReference.document("StudentSequence")
+                // 새로운 Sequence 정보를 생성한다
+                val map = mutableMapOf<String, Long>()
+                map["value"] = sequence.toLong()
+
+                // 새로운 Sequence를 업데이트한다
+                documentReference.set(map).await()
+            } catch (e: Exception){
+                Log.e("ScoreDao", "Sequence 업데이트 실패 : ${e.message}")
+            }
+        }
+
+        // Firebase에 학생 정보를 저장한다
+        suspend fun insertStudentData(studentData: ScoreInfo){
+            try {
+                // 컬랙션에 접근 가능한 객체 생성
+                val collectionReference = Firebase.firestore.collection("ScoreData")
+                // 데이터를 추가한다
+                collectionReference.add(studentData).await()
+            } catch (e: Exception){
+                Log.e("ScoreDao", "학생정보 저장 실패 : ${e.message}")
+            }
+        }
+
+        // studentIdx를 이용해 학생 정보를 불러온다
+        suspend fun getStudentDataByIdx(studentIdx: Int): ScoreInfo?{
+            return try {
+                // 컬랙션에 접근 가능한 객체 생성
+                val collectionReference = Firebase.firestore.collection("ScoreData")
+                // studentIdx가 parameter와 같은 데이터를 불러온다
+                val querySnapshot = collectionReference.whereEqualTo("studentIdx", studentIdx).get().await()
+                // 불러온 데이터를 ScoreInfo에 맞게 바꾸어 반환한다
+                querySnapshot.documents.firstOrNull()?.toObject(ScoreInfo::class.java)
+            } catch (e: Exception){
+                Log.e("ScoreDao", "학생정보 조회 실패 : ${e.message}")
+                null
+            }
+        }
     }
 }
