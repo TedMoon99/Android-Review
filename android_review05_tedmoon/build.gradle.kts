8f1ce50bcef3cb7253e0f5c37980bd1f35ce570a
TedMoon99 <mtj1015@naver.com>
2024-07-15 16:07:26 +0900

    [Feat] Firebase, Firestore 사용을 위한 gradle 추가

--- android_review05_tedmoon/build.gradle.kts
+++ android_review05_tedmoon/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -46,7 +47,10 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.fragment.ktx.v181)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.firestore.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(platform(libs.firebase.bom.v3312))
+    implementation(libs.google.firebase.analytics)
 }
