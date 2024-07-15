eddce100d4423ca31a13958185388062f730c088
TedMoon99 <mtj1015@naver.com>
2024-07-15 15:23:32 +0900

    [Gradle] activityViewModels()를 사용하기 위한 gradle - fragment 추가

--- android_review05_tedmoon/build.gradle.kts
+++ android_review05_tedmoon/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
+    implementation(libs.androidx.fragment.ktx.v181)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
