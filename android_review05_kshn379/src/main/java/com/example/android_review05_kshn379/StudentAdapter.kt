9d0c374a653a31cddf4ea438e28a30d1c3eb6239
KSHN <koorain@naver.com>
2024-07-18 11:13:22 +0900

    [Feat] StudentAdapter - ViewHolder 내 contextMenu 항목 구현

--- android_review05_kshn379/src/main/java/com/example/android_review05_kshn379/StudentAdapter.kt
+++ android_review05_kshn379/src/main/java/com/example/android_review05_kshn379/StudentAdapter.kt
@@ -1,45 +1,44 @@
 package com.example.android_review05_kshn379
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.ContextMenu
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.PopupMenu
 import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android_review05_kshn379.databinding.RowBinding
 
-class StudentAdapter (
-    private val dataSet: ArrayList<ManageInfo>, val manager: FragmentManager
+class StudentAdapter(
+    private val dataSet: ArrayList<ManageInfo>, private val manager: FragmentManager
 ) : RecyclerView.Adapter<StudentAdapter.StoreViewHolder>() {
+
     // ViewHolder class 생성
-    class StoreViewHolder(var rowBinding: RowBinding): RecyclerView.ViewHolder(rowBinding.root),
-        View.OnCreateContextMenuListener {
+    class StoreViewHolder(rowBinding: RowBinding) : RecyclerView.ViewHolder(rowBinding.root) {
+        val rowBinding: RowBinding
+
         init {
-            itemView.setOnCreateContextMenuListener(this)
             this.rowBinding = rowBinding
 
-//            // 항목의 클릭 영역을 항목 자체로 설정한다
-//            rowBinding.root.layoutParams = ViewGroup.LayoutParams(
-//                ViewGroup.LayoutParams.MATCH_PARENT, // 가로 전체 너비 클릭
-//                ViewGroup.LayoutParams.WRAP_CONTENT // 세로 맞춤 높이 클릭
-//            )
+            // 항목의 클릭 영역을 항목 자체로 설정한다
+            rowBinding.root.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, // 가로 전체 너비 클릭
+                ViewGroup.LayoutParams.WRAP_CONTENT // 세로 맞춤 높이 클릭
+            )
 
-//            // 항목을 Long 클릭 시 메뉴를 띄워 준다
-//            rowBinding.root.setOnCreateContextMenuListener { menu, v, menuInfo ->
-//                menu?.add()
-//            }
-
-        }
-        // 리사이클러 뷰 메뉴 설정 및 데이터 연결 예정~~~
-        // ContextMenu(RecyclerView 아이템) 클릭 시 설정
-        override fun onCreateContextMenu(
-            menu: ContextMenu?,
-            v: View?,
-            menuInfo: ContextMenu.ContextMenuInfo?
-        ) {
-            menu?.setHeaderTitle()
-            menuInflater.inflate(R.menu.contextmenu_delete, menu)
+            // 항목을 Long 클릭 시 메뉴를 띄워 준다
+            rowBinding.root.setOnCreateContextMenuListener { menu, v, menuInfo ->
+                menu.apply {
+                    setHeaderTitle("데이터가 삭제됩니다")
+                    // ContextMenu 항목 클릭 시 데이터 삭제 구현
+                    add("삭제").setOnMenuItemClickListener {
+                        ManageInfo.
+                    }
+                }
+            }
         }
 
 
