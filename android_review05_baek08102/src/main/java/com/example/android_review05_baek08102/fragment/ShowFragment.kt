77f0c487cc0fd01334c4461a6586dd3961cc8410
baek08102 <baek2654@gmail.com>
2024-07-21 18:29:45 +0900

    [UI] ShowFragment - 화면 출력 작업 완료

--- android_review05_baek08102/src/main/java/com/example/android_review05_baek08102/fragment/ShowFragment.kt
+++ android_review05_baek08102/src/main/java/com/example/android_review05_baek08102/fragment/ShowFragment.kt
@@ -1,34 +1,81 @@
 package com.example.android_review05_baek08102.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.android_review05_baek08102.R
+import com.example.android_review05_baek08102.dao.StudentDao
 import com.example.android_review05_baek08102.databinding.FragmentShowBinding
+import com.example.android_review05_baek08102.model.StudentData
+import com.example.android_review05_baek08102.utils.FragmentName
+import com.example.android_review05_baek08102.viewmodel.ShowViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class ShowFragment : Fragment() {
     private lateinit var binding: FragmentShowBinding
+    private val data: ArrayList<StudentData> = arrayListOf()
+    private val viewModel: ShowViewModel by activityViewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_show, container, false)
+        binding.showViewModel = viewModel
         binding.lifecycleOwner = this
 
         return binding.root
     }
 
-    fun settingView() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        settingView()
+        settingEvent()
+        settingData()
+    }
+
+    fun settingView() {
+        binding.apply {
+            showToolbar.apply {
+                setTitle("학생 정보 보기")
+                setNavigationIcon(R.drawable.arrow_back_24px)
+            }
+        }
     }
 
     fun settingEvent() {
+        binding.apply {
+            showToolbar.apply {
+                setNavigationOnClickListener {
+                    removeFragment()
+                }
+            }
+        }
+    }
 
+    fun settingData() {
+        viewModel.getData()
+
+        Log.d("showFragment", "getData() 함수 실행 완료")
+    }
+
+    fun removeFragment() {
+        parentFragmentManager.popBackStack(
+            FragmentName.Show_Fragment.name,
+            FragmentManager.POP_BACK_STACK_INCLUSIVE
+        )
     }
 
 }
