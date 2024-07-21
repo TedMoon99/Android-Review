d5fafe2efc6318d0fdcb92522d380964a4e46805
TedMoon99 <mtj1015@naver.com>
2024-07-21 19:54:59 +0900

    [Feat] ScoreInfo - firebase 데이터 조회를 위한 constuctor 추가

--- android_review05_tedmoon/src/main/java/com/example/android_review05_tedmoon/model/ScoreInfo.kt
+++ android_review05_tedmoon/src/main/java/com/example/android_review05_tedmoon/model/ScoreInfo.kt
@@ -8,4 +8,8 @@ data class ScoreInfo(
     val english: Double,
     val math: Double,
     val state: Boolean
-)
+){
+    // firestore를 사용할 때 데이터를 담을 클래스를 지정하게 되면(ScoreInfo::class.java)
+    // 매개변수가 없는 생성자를 사용하여 객체를 생성해주기 때문에 만들어주어야 한다
+    constructor() : this(0, "", 0, 0.0, 0.0, 0.0, false)
+}
