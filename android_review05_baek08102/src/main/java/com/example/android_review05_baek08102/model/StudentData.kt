db47efce8a642f0d2fd5d190ff12b0b123864a6b
baek08102 <baek2654@gmail.com>
2024-07-21 18:19:00 +0900

    [Feat] StudentData - 데이터 출력 여부 나타낼 dataState 멤버 추가

--- android_review05_baek08102/src/main/java/com/example/android_review05_baek08102/model/StudentData.kt
+++ android_review05_baek08102/src/main/java/com/example/android_review05_baek08102/model/StudentData.kt
@@ -8,10 +8,11 @@ data class StudentData(
     val englishScore: Double,
     val mathScore: Double,
     val totalScore: Double,
-    val average: Double
+    val average: Double,
+    val dataState: Boolean
 ) {
     // 매개변수가 없는 생성자
     // firestore를 사용할 때 데이터를 담을 클래스를 지정하게 되면
     // 매개변수가 없는 생성자를 사용하여 객체를 생성해주기 때문에 만들어주어야 한다.
-    constructor() : this(0, "", 0, 0.0, 0.0, 0.0, 0.0, 0.0)
+    constructor() : this(0, "", 0, 0.0, 0.0, 0.0, 0.0, 0.0, true)
 }
