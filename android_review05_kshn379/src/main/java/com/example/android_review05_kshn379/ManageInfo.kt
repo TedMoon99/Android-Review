03980c9f41780104cb17e9c2f85964943468ab4b
kshn379 <94738162+kshn379@users.noreply.github.com>
2024-07-21 14:00:30 +0900

    [Feat] ManageInfo - data class 변수 dataState 값 true 설정

--- android_review05_kshn379/src/main/java/com/example/android_review05_kshn379/ManageInfo.kt
+++ android_review05_kshn379/src/main/java/com/example/android_review05_kshn379/ManageInfo.kt
@@ -6,7 +6,9 @@ data class ManageInfo (
     val studentGrade: Int,
     val studentKor: Double,
     val studentEng: Double,
-    val studentMath: Double
+    val studentMath: Double,
+    // dataState true 로 설정 하여 데이터 표시 하기
+    var dataState: Boolean = true
 )
 {
     constructor() : this(0,"",0,0.0,0.0,0.0)
