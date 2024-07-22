e3c6e4b7b0530b0be1cd36132c50e04fdddb41eb
baek08102 <baek2654@gmail.com>
2024-07-22 19:45:17 +0900

    [Feat] ShowViewModel - 클릭 position 전달 함수 추가 및 상세 주석 추가

--- android_review05_baek08102/src/main/java/com/example/android_review05_baek08102/viewmodel/ShowViewModel.kt
+++ android_review05_baek08102/src/main/java/com/example/android_review05_baek08102/viewmodel/ShowViewModel.kt
@@ -13,6 +13,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class ShowViewModel : ViewModel() {
+    // ShowFragment에서 출력할 데이터
     val name = MutableLiveData<String>()
     val grade = MutableLiveData<String>()
     val koreanScore = MutableLiveData<String>()
@@ -21,16 +22,23 @@ class ShowViewModel : ViewModel() {
     val totalScore = MutableLiveData<String>()
     val average = MutableLiveData<String>()
 
-    val seletedIndex = MutableLiveData<Int>()
+    // adapter로부터 전달받을 데이터. 각각,
+    val seletedIndex = MutableLiveData<Int>() // 클릭한 아이템의 studentIdx 데이터
+    val clickedPosition = MutableLiveData<Int>() // 클릭한 아이템의 position
 
 
+    // Dao 통해 firestore로부터 ShowFragment에 출력할 데이터 받아오는 함수
     fun getData() {
+        // adapter로부터 전달받은 studentIdx 데이터 바탕으로
         seletedIndex.value?.let { index ->
-
+            // viewModel이 살아있는 동안 작업 처리
             viewModelScope.launch {
                 try {
                     val studentData =
                         withContext(Dispatchers.IO) { StudentDao.gettingStudentDataByStudentIndex(index) }
+                    // 백그라운드 스레드에서 작업 실행
+                    // 전달받은 studentIdx 데이터와 부합하는 문서의 필드 데이터 가져옴
+
 
                     if (studentData != null) {
                         name.value = "이름 : ${studentData.name}"
@@ -54,22 +62,33 @@ class ShowViewModel : ViewModel() {
         }
     }
 
-    fun deleteDataOnPosition(position: Int) {
+    // position번째 데이터가 더 이상 화면에 표시되지 않도록 하는 함수(데이터 삭제 작업)
+    fun deleteDataOnPosition() {
+
+        val position = clickedPosition.value // 전달받은 position
 
         Log.d("deleting data process", "in ViewModel -> clicked position data : $position")
 
-        viewModelScope.launch {
-            try {
-                withContext(Dispatchers.IO) {
-                    StudentDao.deleteData(position)
+        if (position != null) {
+            viewModelScope.launch {
+                try {
+                    withContext(Dispatchers.IO) {
+                        StudentDao.deleteData(position) // position 번째의 데이터 삭제 작업 Dao에서 처리
+                    }
+                } catch (e: Exception) {
+                    Log.e("ShowViewModel", "delete Data failed ${e.message}")
                 }
-            } catch (e: Exception) {
-                Log.e("ShowViewModel", "delete Data failed ${e.message}")
             }
         }
     }
 
-    fun updateSelectedIndex(index: Int) {
+    // 사용자가 클릭한 아이템의 studentIdx 데이터 viewModel의 멤버에 저장하는 함수
+    fun updateSelectediIndex(index: Int) {
         seletedIndex.value = index
+    }
+
+    // 사용자가 클릭한 아이템의 position viewModel의 멤버에 저장하는 함수
+    fun updateClickedPosition(position: Int) {
+        clickedPosition.value = position
     }
 }
