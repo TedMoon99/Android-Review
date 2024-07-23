829bd2bd5163a1cf82c05b3efae58531526dbd75
TedMoon99 <mtj1015@naver.com>
2024-07-23 19:32:43 +0900

    [Fix] TotalInfo.kt - 데이터 클래스 파라미터 수정

--- android_review05_tedmoon/src/main/java/com/example/android_review05_tedmoon/model/TotalInfo.kt
+++ android_review05_tedmoon/src/main/java/com/example/android_review05_tedmoon/model/TotalInfo.kt
@@ -2,11 +2,8 @@ package com.example.android_review05_tedmoon.model
 
 data class TotalInfo(
     val studentIdx: Int,
-    val koreanTotal : Double,
-    val englishTotal: Double,
-    val MathTotal: Double,
     val wholeTotal: Double,
     val average: Double
 ){
-    constructor() : this(-1, 0.0, 0.0, 0.0, 0.0, 0.0)
+    constructor() : this(-1, 0.0, 0.0)
 }
