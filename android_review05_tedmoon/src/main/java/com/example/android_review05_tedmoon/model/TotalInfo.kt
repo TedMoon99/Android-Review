bdc0f6b1949589bcc7484b2f8c15c0d17e482f04
TedMoon99 <mtj1015@naver.com>
2024-07-24 18:40:21 +0900

    [Feat] TotalInfo.kt - 글 상태 parameter 추가

--- android_review05_tedmoon/src/main/java/com/example/android_review05_tedmoon/model/TotalInfo.kt
+++ android_review05_tedmoon/src/main/java/com/example/android_review05_tedmoon/model/TotalInfo.kt
@@ -3,7 +3,8 @@ package com.example.android_review05_tedmoon.model
 data class TotalInfo(
     val studentIdx: Int,
     val wholeTotal: Double,
-    val average: Double
+    val average: Double,
+    val state: Boolean
 ){
-    constructor() : this(-1, 0.0, 0.0)
+    constructor() : this(-1, 0.0, 0.0, true)
 }
