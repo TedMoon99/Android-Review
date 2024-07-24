b8a1021d71e36485276c9005c83a64b0a2974d97
TedMoon99 <mtj1015@naver.com>
2024-07-24 18:39:59 +0900

    [Feat] Tools.kt - 글 상태 enum class 생성

--- android_review05_tedmoon/src/main/java/com/example/android_review05_tedmoon/utils/Tools.kt
+++ android_review05_tedmoon/src/main/java/com/example/android_review05_tedmoon/utils/Tools.kt
@@ -11,3 +11,9 @@ enum class FragmentName(var str: String){
     SHOW_FRAGMENT("ShowFragment"),
 
 }
+
+// 정보 상태를 나타내는 값을 정의한다
+enum class ScoreDataState(var str: String, var state: Boolean){
+    SCORE_DATA_STATE_NORMAL("정상", true),
+    SCORE_DATA_STATE_DELETED("삭제", false),
+}
