315fab9e114cb250363665932ed3057a02f1d8fc
kshn379 <94738162+kshn379@users.noreply.github.com>
2024-07-25 12:55:58 +0900

    [Feat] Tools - enum class 함수 사용하여 화면 이동 구현

---
+++ android_review06_kshn379/src/main/java/com/example/android_review06_kshn379/Tools.kt
@@ -0,0 +1,11 @@
+package com.example.android_review06_kshn379
+
+class Tools {
+    companion object {
+
+    }
+}
+
+enum class FragmentName (var str: String) {
+    ADD_FRAGMENT("AddFragment"),
+}
