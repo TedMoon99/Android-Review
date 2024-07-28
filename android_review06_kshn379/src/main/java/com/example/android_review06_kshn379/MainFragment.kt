9a343d0cab0c32fccddcd9a816664cbe07d78a36
kshn379 <94738162+kshn379@users.noreply.github.com>
2024-07-28 14:03:46 +0900

    [Feat] MainFragment - data class 설정

--- android_review06_kshn379/src/main/java/com/example/android_review06_kshn379/MainFragment.kt
+++ android_review06_kshn379/src/main/java/com/example/android_review06_kshn379/MainFragment.kt
@@ -1,6 +1,7 @@
 package com.example.android_review06_kshn379
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -15,7 +16,7 @@ import com.google.android.material.divider.MaterialDividerItemDecoration
 class MainFragment : Fragment() {
 
     private lateinit var binding: FragmentMainBinding
-//    private val animalList: ArrayList<ZooInfo> = arrayListOf()
+    private val animalList: ArrayList<ZooInfo> = arrayListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -131,6 +132,8 @@ class MainFragment : Fragment() {
                     .addToBackStack(name.str)
                     .commit()
             }
+
+           else -> Log.d("wrong page", "Page 404 Error")
         }
     }
 
