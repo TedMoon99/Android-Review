8b28809b4dab03fa86117e659e3ee02625691cf3
kshn379 <94738162+kshn379@users.noreply.github.com>
2024-07-28 14:04:57 +0900

    [Feat] AddViewModel - ViewModel 설정 및 입력 요소 초기화

---
+++ android_review06_kshn379/src/main/java/com/example/android_review06_kshn379/AddViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.android_review06_kshn379
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class AddViewModel : ViewModel() {
+    val animalName = MutableLiveData<String>()
+    val animalAge = MutableLiveData<String>()
+    val animalCount = MutableLiveData<String>()
+    val animalDetail = MutableLiveData<String>()
+
+
+    fun clearText() {
+        animalName.value = ""
+        animalAge.value = ""
+        animalCount.value = ""
+        animalDetail.value = ""
+    }
+}
