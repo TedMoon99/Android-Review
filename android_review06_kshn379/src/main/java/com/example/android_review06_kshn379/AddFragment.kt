5e3b390cb87cb407340c518533b6e3af348b3caf
kshn379 <94738162+kshn379@users.noreply.github.com>
2024-07-28 14:05:30 +0900

    [Feat] AddFragment - Error 설정 및 유효성 검사 설정

--- android_review06_kshn379/src/main/java/com/example/android_review06_kshn379/AddFragment.kt
+++ android_review06_kshn379/src/main/java/com/example/android_review06_kshn379/AddFragment.kt
@@ -5,23 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.core.view.isGone
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.activityViewModels
 import com.example.android_review06_kshn379.databinding.FragmentAddBinding
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class AddFragment : Fragment() {
 
     private lateinit var binding: FragmentAddBinding
-//    private val viewModel: AddViewModel by activityViewModels()
+    private val viewModel: AddViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_add, container, false)
-//        binding.addViewModel = viewModel
+        binding.addViewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
 
         return binding.root
@@ -35,7 +42,12 @@ class AddFragment : Fragment() {
         // Event 설정
         settingEvent()
         // 입력 요소 초기화
+        settingInput()
+    }
 
+    // 입력 요소 초기화
+    private fun settingInput() {
+        viewModel.clearText()
     }
 
     // View 설정
@@ -50,6 +62,63 @@ class AddFragment : Fragment() {
             // Toolbar 설정
             toolBarAdd.apply {
                 inflateMenu(R.menu.menu_items)
+            }
+        }
+        // Error 설정
+        settingError()
+    }
+
+    // Error 설정
+    fun settingError() {
+        binding.apply {
+            // name
+            viewModel.animalName.observe(viewLifecycleOwner) { name ->
+                if (name != null && name.isNotEmpty()) {
+                    if (name.length in 2..7) {
+                        editTextAnimalName.error = null
+                    } else {
+                        editTextAnimalName.error = "동물 이름은 6자 이하로 입력 하세요"
+                    }
+                } else {
+                    editTextAnimalName.error = null
+                }
+            }
+            // age
+            viewModel.animalAge.observe(viewLifecycleOwner) { age ->
+                if (age != null && age.isNotEmpty()) {
+                    if (age.toInt() in 1..100) {
+                        editTextAnimalAge.error = null
+                    } else {
+                        editTextAnimalAge.error = "동물 나이는 100살 이하로 입력해 주세요"
+                    }
+                } else {
+                    editTextAnimalAge.error = null
+                }
+            }
+
+            // count
+            viewModel.animalCount.observe(viewLifecycleOwner) { count ->
+                if (count != null && count.isNotEmpty()) {
+                    if (count.toInt() in 1..100) {
+                        editTextAnimalCount.error = null
+                    } else {
+                        editTextAnimalCount.error = "0~100 사이의 값을 입력해 주세요"
+                    }
+                } else {
+                    editTextAnimalCount.error = null
+                }
+            }
+            // detail
+            viewModel.animalDetail.observe(viewLifecycleOwner) { detail ->
+                if (detail != null && detail.isNotEmpty()) {
+                    if (detail.length in 2..10) {
+                        editTextAnimalDetail.error = null
+                    } else {
+                        editTextAnimalDetail.error = "1~10 사이의 값을 입력해 주세요"
+                    }
+                } else {
+                    editTextAnimalDetail.error = null
+                }
             }
         }
     }
@@ -141,17 +210,82 @@ class AddFragment : Fragment() {
                 setOnMenuItemClickListener { menu ->
                     when (menu.itemId) {
                         R.id.menuitems_item_complete -> {
-                            // snackbar message 출력
-                            Snackbar.make(binding.root, "동물 정보가 등록되었습니다.", Snackbar.LENGTH_SHORT)
-                                .show()
-                            // 뒤로 가기
-                            removeFragment()
+                            // 유효성 검사
+                            val result = validateInput()
+                            if (result) {
+                                // Data 저장
+//                                saveData()
+
+                                // snackbar message 출력
+                                Snackbar.make(
+                                    binding.root,
+                                    "동물 정보가 등록되었습니다.",
+                                    Snackbar.LENGTH_SHORT
+                                )
+                                    .show()
+                            } else {
+                                popErrorDialog()
+                            }
                         }
                     }
                     true
                 }
             }
         }
+    }
+
+    // 유효성 검사
+    fun validateInput(): Boolean {
+        // 입력 요소 가져오기
+        val name = viewModel.animalName.value ?: ""
+        val age = viewModel.animalAge.value ?: ""
+        val count = viewModel.animalCount.value ?: ""
+        val detail = viewModel.animalDetail.value ?: ""
+
+        // Animal name
+        if (name.isEmpty() || name.length < 2 || name.length > 7) {
+            return false
+        }
+
+        // Animal age
+        if (age.isEmpty() || age.toIntOrNull() == null || age.toInt() < 1 || age.toInt() > 100) {
+            return false
+        }
+
+        // Animal count
+        if (count.isEmpty() || count.toIntOrNull() == null || count.toInt() < 1 || count.toInt() > 100) {
+            return false
+        }
+
+        // Animal detail
+        if (detail.isEmpty() || detail.length < 2 || detail.length > 10) {
+            return false
+        }
+
+        return true
+
+    }
+
+//    // Data 저장
+//    private fun saveData() {
+//        CoroutineScope(Dispatchers.Main).launch {
+//            try {
+//                // animalIdx 불러오기
+//                val animalSequence = withContext(Dispatchers.IO) { AddDao.getSequence()}
+//
+//            }
+//        }
+//    }
+
+    // Error Dialog 설정
+    fun popErrorDialog() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("다시 확인 해주세요")
+            .setMessage("모든 정보가 입력되지 않았습니다!")
+            // 아이콘 설정
+            .setPositiveButtonIcon(ContextCompat.getDrawable(requireContext(), R.drawable.animal))
+            .setPositiveButton("확인") {dialog, _ -> dialog.dismiss()}
+            .show()
     }
 
     // 뒤로 가기 설정
