189713364e3889a527e21036d82689e657001fc6
kshn379 <94738162+kshn379@users.noreply.github.com>
2024-08-01 12:57:43 +0900

    [Feat] EditFragment - RecyclerView Item 수정 데이터 유효성 검사 및 Dialog 구현

--- android_review06_kshn379/src/main/java/com/example/android_review06_kshn379/EditFragment.kt
+++ android_review06_kshn379/src/main/java/com/example/android_review06_kshn379/EditFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.activityViewModels
@@ -62,17 +64,72 @@ class EditFragment : Fragment() {
                 setOnMenuItemClickListener { rename ->
                     when (rename.itemId) {
                         R.id.menuitem_rename_edit -> {
+                            // 유효성 검사
+                            val check = editInput()
+                            if (check) {
+                                // Edit Data 저장
+                                editData()
+                            }
                             // SnackBar Message 출력
-                            Snackbar.make(binding.root, "동물 정보가 수정되었습니다", Snackbar.LENGTH_SHORT)
+                            Snackbar.make(binding.root, "동물 정보가 변경 되었습니다", Snackbar.LENGTH_SHORT)
                                 .show()
                             // 뒤로 가기
                             removeFragment()
+                        } else -> {
+                            editErrorDialog()
                         }
                     }
                     true
                 }
             }
         }
+    }
+
+    // RecyclerView Item 데이터 수정 유효성 검사
+    private fun editInput(): Boolean {
+        // 입력 요소 가져오기
+        val animalName = viewModel.animalName.value ?: ""
+        val animalAge = viewModel.animalAge.value ?: ""
+        val animalCount = viewModel.animalCount.value ?: ""
+        val animalDetail = viewModel.animalDetail.value ?: ""
+
+        // name
+        if (animalName.isNotEmpty() || animalName.length < 2 || animalName.length > 7) {
+            return false
+        }
+        // Animal age
+        if (animalAge.isEmpty() || animalAge.toIntOrNull() == null || animalAge.toInt() < 1 || animalAge.toInt() > 100) {
+            return false
+        }
+
+        // Animal count
+        if (animalCount.isEmpty() || animalCount.toIntOrNull() == null || animalCount.toInt() < 1 || animalCount.toInt() > 100) {
+            return false
+        }
+
+        // Animal detail
+        if (animalDetail.isEmpty() || animalDetail.length < 2 || animalDetail.length > 10) {
+            return false
+        }
+
+        return true
+
+    }
+
+    // Error Dialog 설정
+    private fun editErrorDialog() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("다시 확인 해주세요")
+            .setMessage("모든 정보가 입력되지 않았습니다!")
+            // 아이콘 설정
+            .setPositiveButtonIcon(ContextCompat.getDrawable(requireContext(), R.drawable.animal))
+            .setPositiveButton("확인") { dialog, _ -> dialog.dismiss() }
+            .show()
+    }
+
+    // Edit Data 저장
+    private fun editData() {
+
     }
 
     // 뒤로 가기 설정
