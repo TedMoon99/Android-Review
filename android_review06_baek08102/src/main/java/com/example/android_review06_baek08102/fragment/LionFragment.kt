595f5c4d7c216c256ad80d5eb6ec4f8ce5d95d3e
baek08102 <baek2654@gmail.com>
2024-08-03 19:37:02 +0900

    [Comment] LionFragment - 각 동작 부분 상세 주석 추가 / 작업 완료

--- android_review06_baek08102/src/main/java/com/example/android_review06_baek08102/fragment/LionFragment.kt
+++ android_review06_baek08102/src/main/java/com/example/android_review06_baek08102/fragment/LionFragment.kt
@@ -29,6 +29,10 @@ import kotlinx.coroutines.withContext
 class LionFragment : Fragment(), InputFragment.DataInputListener {
     private lateinit var binding: FragmentLionBinding
     private val viewModel: LionViewModel by activityViewModels()
+
+    // 해당 Fragment가 이미 attach 되었는지 확인
+    // InputFragment의 fragment container에 처음으로 담기는 시점에서만
+    // 이전에 입력해둔 InputData 초기화 시켜주기 위해 사용되는 값
     private var isInitialized = false
 
     override fun onCreateView(
@@ -43,6 +47,8 @@ class LionFragment : Fragment(), InputFragment.DataInputListener {
         return binding.root
     }
 
+    // 프래그먼트가 다시 보이게 될 때마다 에러와 드롭다운을 새로 설정
+    // 자세한 건 Readme에
     override fun onStart() {
         super.onStart()
 
@@ -56,6 +62,7 @@ class LionFragment : Fragment(), InputFragment.DataInputListener {
         settingView()
     }
 
+    // 뷰 설정
     fun settingView() {
         binding.apply {
             emptyInput()
@@ -64,6 +71,7 @@ class LionFragment : Fragment(), InputFragment.DataInputListener {
 
     // 입력값 유효성검사
     fun validateInput(): Boolean {
+        // viewModel로부터 사용자가 입력한 데이터 받아와서
         val name = viewModel.lionName.value ?: ""
         val age = viewModel.lionAge.value ?: ""
         val hairCount = viewModel.hairCount.value ?: ""
@@ -71,6 +79,7 @@ class LionFragment : Fragment(), InputFragment.DataInputListener {
 
         Log.d("validateInput", "name : $name, age : $age, hairCount : $hairCount, sex : $sex")
 
+        // 각 부분에서 검사 진행 후 통과하지 못한다면 false 리턴 후 함수 종료
         if (name.isEmpty() || name.length < 1 || name.length > 10) {
             return false
         }
@@ -83,13 +92,20 @@ class LionFragment : Fragment(), InputFragment.DataInputListener {
         if (sex.isEmpty()) {
             return false
         }
+
+        // 순차적으로 진행되는 모든 검사를 통과한 후에 true 리턴 후 함수 종료
         return true
     }
 
+    // 에러 설정
     fun settingError() {
         binding.apply {
 
+            // 사용자가 값을 입력한 후부터 에러 출력
             lionInputEdittextName.doAfterTextChanged {
+
+                // 실시간 입력값 관측하여
+                // 각 조건에 맞는 에러 출력
                 viewModel.lionName.observe(viewLifecycleOwner) { name ->
                     if (name.isNotEmpty() && name != null) {
                         if (name.length in 1..10) {
@@ -130,33 +146,28 @@ class LionFragment : Fragment(), InputFragment.DataInputListener {
                     }
                 }
             }
-
-            lionAutoCompleteTextViewSex.doAfterTextChanged {
-                viewModel.sex.observe(viewLifecycleOwner) { sex ->
-                    if (sex.isNotEmpty() && sex != null) {
-                        lionTextInputLayoutSex.error = null
-                    } else {
-                        lionTextInputLayoutSex.error = "성별을 입력해주세요"
-                    }
-                }
-            }
         }
     }
 
+    // 입력값 저장
     fun saveInput() {
         CoroutineScope(Dispatchers.Main).launch {
             try {
-
+                // 현재 시퀀스값 받아와서
                 val animalSequence = withContext(Dispatchers.IO) { AnimalDao.getSequence() }
+                // 데이터 저장 시마다 업데이트 해주고
                 withContext(Dispatchers.IO) { AnimalDao.updateSequence(animalSequence + 1) }
 
+                // 시퀀스 + 1로 인덱스값 설정
                 val index = animalSequence + 1
 
+                // 나이 제외한 모든 값 입력 시에는 String 타입으로
                 val lionName = viewModel.lionName.value ?: ""
                 val lionAge = viewModel.lionAge.value?.toInt() ?: 0
                 val hairCount = viewModel.hairCount.value ?: ""
                 val sex = viewModel.sex.value ?: ""
 
+                // 각 동물들 모두 AnimalData 타입으로 통일하여 입력
                 val inputData = AnimalData(0, lionName, lionAge, hairCount, sex, index, true)
 
                 withContext(Dispatchers.IO) { AnimalDao.saveAnimalData(inputData) }
@@ -168,6 +179,8 @@ class LionFragment : Fragment(), InputFragment.DataInputListener {
 
     }
 
+    // 인스턴스 재생성 시
+    // 기존에 viewModel에 남아있던 입력값 초기화
     fun emptyInput() {
         if (!isInitialized) {
             viewModel.apply {
@@ -189,11 +202,12 @@ class LionFragment : Fragment(), InputFragment.DataInputListener {
         }
     }
 
-    //
+    // InputFragment에서 onSaveRequest 호출 시 saveInput() 실행
     override fun onSaveRequest() {
         saveInput()
     }
 
+    // InputFragment에 입력값의 유효성 검사 결과 전달
     override fun isDataValidate(): Boolean {
         return validateInput()
     }
