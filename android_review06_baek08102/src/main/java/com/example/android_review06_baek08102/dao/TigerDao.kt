956869815ec1e33026231013f26a07bb037c270c
baek08102 <baek2654@gmail.com>
2024-08-11 18:45:52 +0900

    [Feat] Dao - 각 동물 별 Dao 생성 및 데이터 입력 작업 추가로 처리하도록 변경 > 기존에 AnimalDao에서 AnimalData 타입으로 세 동물 타입 통일하여 하나의 콜렉션으로만 입력받던 방식에서 기존의 방식 + 각 동물 별 콜렉션 추가 생성 및 데이터 입력하는 방식으로 변경

---
+++ android_review06_baek08102/src/main/java/com/example/android_review06_baek08102/dao/TigerDao.kt
@@ -0,0 +1,56 @@
+package com.example.android_review06_baek08102.dao
+
+import android.util.Log
+import com.example.android_review06_baek08102.model.TigerData
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import kotlinx.coroutines.tasks.await
+
+class TigerDao {
+    companion object {
+
+        // 시퀀스 값 불러오는 함수
+        suspend fun getSequence(): Int {
+            return try {
+                // Sequence 콜렉션 참조
+                val collectionReference = Firebase.firestore.collection("Sequence")
+                // animalSequence 문서 참조
+                val documentReference = collectionReference.document("animalSequence")
+                // 참조한 animalSequence 문서 스냅샷
+                val documentSnapShot = documentReference.get().await()
+
+                // animalSequence 문서의 value 필드의 값 Int 타입으로 반환 시도
+                documentSnapShot.getLong("value")?.toInt() ?: -1
+            } catch (e: Exception) {
+                Log.e("AnimalDao", "Sequence 조회 실패 : ${e.message}")
+            }
+        }
+
+        // 시퀀스 값 업데이트 함수
+        suspend fun updateSequence(animalSequence: Int) {
+            try { // Sequence 콜렉션 참조
+                val collectionReference = Firebase.firestore.collection("Sequence")
+                // animalSequence 문서 참조
+                val documentReference = collectionReference.document("animalSequence")
+
+                // String 타입 key, Long 타입 값의 구조를 가진 비어있는 mutableMap
+                val map = mutableMapOf<String, Long>()
+                // value라는 키 값으로 인자로 받은 animalSequence Long 타입 변환 후 입력
+                map["value"] = animalSequence.toLong()
+                // 참조한 문서에 map 저장
+                documentReference.set(map).await()
+            } catch (e: Exception) {
+                Log.e("AnimalDao", "Sequence 업데이트 실패 : ${e.message}")
+            }
+        }
+
+        suspend fun saveTigerData(tigerData: TigerData) {
+            try {
+                val collectionReference = Firebase.firestore.collection("TigerData")
+                collectionReference.add(tigerData).await()
+            } catch (e: Exception) {
+                Log.e("AnimalDao", "saveLionData failed : ${e.message}")
+            }
+        }
+    }
+}
