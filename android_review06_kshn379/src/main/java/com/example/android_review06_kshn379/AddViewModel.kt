1d966369b8936eab55dd06346decec02114a28e8
kshn379 <koorain@naver.com>
2024-08-12 14:00:26 +0900

    [Rename] AddViewModel - Import CoroutineScope

--- android_review06_kshn379/src/main/java/com/example/android_review06_kshn379/AddViewModel.kt
+++ android_review06_kshn379/src/main/java/com/example/android_review06_kshn379/AddViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
