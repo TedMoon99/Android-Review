de1877d1eb8fd550d5437780ab0df533a52b6490
kshn379 <koorain@naver.com>
2024-08-12 14:04:11 +0900

    [Feat] MainFragment - navigation Icon Toast 메세지 설정

--- android_review06_kshn379/src/main/java/com/example/android_review06_kshn379/MainFragment.kt
+++ android_review06_kshn379/src/main/java/com/example/android_review06_kshn379/MainFragment.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -206,6 +207,10 @@ class MainFragment : Fragment() {
                         }
                     }
                     true
+                }
+                // navigation Icon 설정
+                setNavigationOnClickListener {
+                    Toast.makeText(context, "Zoo Data Store", Toast.LENGTH_LONG).show()
                 }
             }
         }
