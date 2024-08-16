30dead829301cf4bb1f192b6d2bbdf0ad3b6af02
baek08102 <baek2654@gmail.com>
2024-08-16 20:28:06 +0900

    [Remove] InputFragment - 불필요한 import문 삭제

--- android_review06_baek08102/src/main/java/com/example/android_review06_baek08102/fragment/InputFragment.kt
+++ android_review06_baek08102/src/main/java/com/example/android_review06_baek08102/fragment/InputFragment.kt
@@ -1,23 +1,16 @@
 package com.example.android_review06_baek08102.fragment
 
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import android.widget.ToggleButton
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.replace
 import androidx.fragment.app.setFragmentResult
-import androidx.fragment.app.setFragmentResultListener
 import com.example.android_review06_baek08102.R
 import com.example.android_review06_baek08102.databinding.FragmentInputBinding
-import com.example.android_review06_baek08102.model.AnimalData
-import com.example.android_review06_baek08102.model.LionData
 import com.example.android_review06_baek08102.utils.ChildFragmentName
 import com.example.android_review06_baek08102.utils.FragmentName
 import com.google.android.material.snackbar.Snackbar
