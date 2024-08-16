8457763b3a000527454afabd69a2dcd6776bcce4
baek08102 <baek2654@gmail.com>
2024-08-16 20:23:41 +0900

    [Feat] CustomAdapter - 리사이클러 아이템 클릭 시 기존 animalIdx만 전달해주던 방식에서 animalType도 함께 전달해주도록 변경

--- android_review06_baek08102/src/main/java/com/example/android_review06_baek08102/adapter/CustomAdapter.kt
+++ android_review06_baek08102/src/main/java/com/example/android_review06_baek08102/adapter/CustomAdapter.kt
@@ -1,21 +1,18 @@
 package com.example.android_review06_baek08102.adapter
 
 import android.view.LayoutInflater
-import android.view.MenuInflater
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android_review06_baek08102.R
-import com.example.android_review06_baek08102.databinding.FragmentMainBinding
 import com.example.android_review06_baek08102.databinding.RowMainBinding
 import com.example.android_review06_baek08102.fragment.ShowFragment
-import com.example.android_review06_baek08102.model.AnimalData
+import com.example.android_review06_baek08102.model.UnifiedAnimalData
 import com.example.android_review06_baek08102.utils.FragmentName
 import com.example.android_review06_baek08102.viewmodel.ShowViewModel
 
 class CustomAdapter(
-    private val dataList: ArrayList<AnimalData>,
+    private val dataList: ArrayList<UnifiedAnimalData>,
     private val fragmentManager: FragmentManager,
     private val viewModel: ShowViewModel
 ) : RecyclerView.Adapter<CustomAdapter.CustomViewHolder>() {
@@ -34,9 +31,14 @@ class CustomAdapter(
         fun clickListener(
             viewModel: ShowViewModel,
             fragmentManager: FragmentManager,
-            animalIdx: Int
+            animalIdx: Int,
+            animalType: Int
         ) {
-            viewModel.updateClickedIndex(animalIdx)
+            viewModel.apply {
+                updateClickedIndex(animalIdx)
+                updateClickedAnimalType(animalType)
+            }
+
 
             fragmentManager.beginTransaction()
                 .replace(R.id.main_container, ShowFragment())
@@ -65,7 +67,12 @@ class CustomAdapter(
         // 아이템 클릭 시
         holder.itemView.setOnClickListener {
             // 사용자 정의 클릭 리스너 함수 통하여 클릭한 item의 animalIdx 전달
-            holder.clickListener(viewModel, fragmentManager, dataList[position].animalIdx)
+            holder.clickListener(
+                viewModel,
+                fragmentManager,
+                dataList[position].animalIdx,
+                dataList[position].animalType.num
+            )
         }
     }
 }
